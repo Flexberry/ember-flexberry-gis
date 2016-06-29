df463de3fe55440ec39cb3b2b8b7ebb1eb2fd3a2
hvostya <tosotova@ics.perm.ru>
2016-06-29 17:50:38 +0500

    Add opportunity to observe changes on layer setting model

--- addon/mixins/new-platform-flexberry-g-i-s-map-layer-setting-route.js
+++ addon/mixins/new-platform-flexberry-g-i-s-map-layer-setting-route.js
@@ -3,9 +3,10 @@ import Ember from 'ember';
 export default Ember.Mixin.create({
   _store: Ember.inject.service('store'),
 
+  _settingRecord: undefined,
+
   actions: {
     renderMainTemplate(layerType, objectToRender, renderInto) {
-      Ember.assert('layerType is not defined', layerType);
       Ember.assert('objectToRender is not defined', objectToRender);
       Ember.assert('renderInto is not defined', renderInto);
 
@@ -28,12 +29,47 @@ export default Ember.Mixin.create({
         controller: 'new-platform-flexberry-g-i-s-map-layer-setting'
       };
 
+      this._clearOldRecord();
+
       if (modelName) {
         let modelToRender = this._formModelByObject(objectToRender, modelName);
         renderOptions.model = modelToRender;
+        this.set('_settingRecord', modelToRender);
+
+        let modelConstructor = modelToRender.constructor;
+        Ember.get(modelConstructor, 'attributes').forEach((attribute) => {
+          this.addObserver('_settingRecord.' + attribute.name, this, this._createdModelChange);
+        });
       }
 
       this.render(templateName, renderOptions);
+    }
+  },
+
+  resetController(controller, isExiting, transition) {
+    this._super(...arguments);
+
+    this._clearOldRecord();
+  },
+
+  _clearOldRecord() {
+    let oldRecord = this.get('_settingRecord');
+    if (oldRecord) {
+      let modelConstructor = oldRecord.constructor;
+      Ember.get(modelConstructor, 'attributes').forEach((attribute) => {
+        this.removeObserver('_settingRecord.' + attribute.name, this, this._createdModelChange);
+      });
+
+      this.set('_settingRecord', undefined);
+      oldRecord.destroyRecord();
+    }
+  },
+
+  _createdModelChange() {
+    let _settingRecord = this.get('_settingRecord');
+    if (_settingRecord) {
+      let changedObject = this._formObjectByModel(_settingRecord);
+      alert('test');
     }
   },
 
@@ -43,5 +79,15 @@ export default Ember.Mixin.create({
 
     let store = this.get('_store');
     return store.createRecord(modelType, currentObject);
+  },
+
+  _formObjectByModel(currentModel) {
+    let modelConstructor = currentModel.constructor;
+    let result = {};
+    Ember.get(modelConstructor, 'attributes').forEach((attribute) => {
+      result[attribute.name] = currentModel.get(attribute.name);
+    });
+
+    return result;
   }
 });
