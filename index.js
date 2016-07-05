ee0cac14738756f90a2a5f33229114751b4031ea
kuzkok <kuzkok@gmail.com>
2016-07-05 08:46:07 +0100

    Remove ember-leaflet addon

--- index.js
+++ index.js
@@ -2,5 +2,17 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-flexberry-gis'
+  name: 'ember-flexberry-gis',
+  included(app) {
+    app.import(app.bowerDirectory + '/leaflet/dist/leaflet-src.js');
+
+    app.import(app.bowerDirectory + '/leaflet/dist/leaflet.css');
+
+    var imagesDestDir = '/assets/images';
+    app.import(app.bowerDirectory + '/leaflet/dist/images/layers-2x.png', { destDir: imagesDestDir });
+    app.import(app.bowerDirectory + '/leaflet/dist/images/layers.png', { destDir: imagesDestDir });
+    app.import(app.bowerDirectory + '/leaflet/dist/images/marker-icon-2x.png', { destDir: imagesDestDir });
+    app.import(app.bowerDirectory + '/leaflet/dist/images/marker-icon.png', { destDir: imagesDestDir });
+    app.import(app.bowerDirectory + '/leaflet/dist/images/marker-shadow.png', { destDir: imagesDestDir });
+  }
 };
