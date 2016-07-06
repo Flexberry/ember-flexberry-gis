6f7ddd85a747bdc3a55896dfe0536c68e34fdfbf
kuzkok <kuzkok@gmail.com>
2016-07-06 12:52:56 +0100

    Fix test errors

--- tests/integration/components/flexberry-map-test.js
+++ tests/integration/components/flexberry-map-test.js
@@ -1,24 +1,23 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import Ember from 'ember';
 
 moduleForComponent('flexberry-map', 'Integration | Component | flexberry map', {
   integration: true
 });
 
 test('it renders', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
+  assert.expect(0);
 
-  this.render(hbs`{{flexberry-map}}`);
+  this.set('model', Ember.Object.create());
 
-  assert.equal(this.$().text().trim(), '');
+  this.render(hbs`{{flexberry-map model=model}}`);
 
   // Template block usage:
   this.render(hbs`
-    {{#flexberry-map}}
+    {{#flexberry-map model=model}}
       template block text
     {{/flexberry-map}}
   `);
 
-  assert.equal(this.$().text().trim(), 'template block text');
 });
