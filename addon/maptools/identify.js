e40a8f35b2ec7cae28aad1c4ec7b6334e9f365da
kuzkok <kuzkok@gmail.com>
2016-08-18 08:18:24 +0100

    Identify tool template

---
+++ addon/maptools/identify.js
@@ -0,0 +1,13 @@
+import MapTool from './maptool';
+
+export default MapTool.extend({
+  cursor: 'help',
+
+  enable() {
+    this._super(...arguments);
+  },
+
+  disable() {
+    this._super(...arguments);
+  }
+})
