f6c0c84b1f769a5057111dbf348f6a90b0c7aad6
Serega Matin <seregamatin@gmail.com>
2016-08-26 17:47:31 +0500

    Add create & remove layer handlers

--- addon/mixins/flexberry-map-load-layers.js
+++ addon/mixins/flexberry-map-load-layers.js
@@ -21,7 +21,7 @@ export default Ember.Mixin.create({
     return new Ember.RSVP.Promise((resolve, reject) => {
       if (layer.get('type') === 'group') {
         this._loadChildLayers(layer)
-          .then(layers => layer.set('layers', layers))
+          .then(layers => layer.set('layers', Ember.A(layers)))
           .then(() => resolve(layer))
           .catch(reason => reject(reason));
       }
@@ -79,7 +79,7 @@ export default Ember.Mixin.create({
       baseModel = map;
       return this._loadChildLayers(map.get('rootLayer'));
     }).then(layers => {
-      baseModel.set('layers', layers);
+      baseModel.set('layers', Ember.A(layers));
       this._setIndex(layers, { index: 0 });
       return mapPromise;
     });
--- tests/dummy/app/controllers/map.js
+++ tests/dummy/app/controllers/map.js
@@ -1,3 +1,4 @@
+import Ember from 'ember';
 import EditFormController from 'ember-flexberry/controllers/edit-form';
 import FlexberryMaplayerActionsHandlerMixin from 'ember-flexberry-gis/mixins/flexberry-maplayer-actions-handler';
 
@@ -12,5 +13,65 @@ export default EditFormController.extend(FlexberryMaplayerActionsHandlerMixin, {
     leafletMapDidInit(leafletMap) {
       this.set('leafletMap', leafletMap);
     }
+  },
+
+  /**
+    Creates new layer as specified layer's child
+    (overridden method from {{#crossLink "FlexberryMaplayerActionsHandlerMixin:_createLayer:method"}}
+    FlexberryMaplayerActionsHandlerMixin
+    {{/crossLink}}).
+
+    @method _createLayer
+    @param {Object} options Method options.
+    @param {String} options.parentLayerPath Path to parent layer.
+    @param {String} options.parentLayer Parent layer.
+    @param {Object} options.layerProperties Object containing new layer properties.
+    @returns {Object} Created layer.
+    @private
+  */
+  _createLayer(options) {
+    options = options || {}; 
+    let parentLayer = Ember.get(options, 'parentLayer');
+    let layerProperties = Ember.get(options, 'layerProperties');
+
+    let store = this.get('store');
+    let layer = store.createRecord('new-platform-flexberry-g-i-s-map-layer', layerProperties);
+    parentLayer.set('parent', layer);
+
+    return layer;
+  },
+
+  /**
+    Removes specified layer from layers hierarchy
+    (overridden method from {{#crossLink "FlexberryMaplayerActionsHandlerMixin:/_removeLayer:method"}}
+    FlexberryMaplayerActionsHandlerMixin
+    {{/crossLink}}).
+
+    @method _removeLayer
+    @param {Object} options Method options.
+    @param {String} options.layerPath Path to removing layer.
+    @param {String} options.layer Removing layer itself.
+    @returns {Object} Removed layer.
+    @private
+  */
+  _removeLayer(options) {
+    options = options || {};
+    let layer = Ember.get(options, 'layer');
+    
+    if (!Ember.isNone(layer)) {
+      Ember.set(layer, 'visibility', false);
+      layer.deleteRecord();
+    }
+
+    let layers = Ember.get(layer, 'layers');
+    if (Ember.isArray(layers)) {
+      layers.forEach((childLayer) => {
+        this._removeLayer({
+          layer: childLayer
+        });
+      });
+    }
+
+    return layer;
   }
 });
