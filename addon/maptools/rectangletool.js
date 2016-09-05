279135c301a736f3e7bcea6f4e1f854b6c34c10c
kaf <kaf@nevod.ru>
2016-09-05 15:53:23 +0500

    Remain rectagletool, rename <figure>tool to <figure>masuretool

--- addon/maptools/rectangletool.js
+++ addon/maptools/rectangletool.js
@@ -3,35 +3,31 @@ import MapTool from 'ember-flexberry-gis/maptools/maptool';
 export default MapTool.extend({
 
   _layer: null,
-  _measure: null,
 
-  rectangleClose(e) {
-    this.rectangleLayer.disableEdit();
-    this.get('map').off('measure:created');
-    this.rectangleLayer.remove();
-  },
-
-  rectangleMeasured(e) {
-    this.rectangleLayer = e.layer;
-    //     e.layer.disableEdit();
-    this.get('map').off('measure:created');
+  rectangleStarted({ layer }) {
+    layer.disableEdit();
+    layer.remove();
+    this._drawRectangle();
   },
 
   enable() {
     this._super(...arguments);
     // there are problems if dragging is disabled
-    var map = this.get('map');
-    map.dragging.enable();
-    this._measure = L.measureBase(map);
-    this._measure.rectangleBaseTool.startMeasure();
-    this.get('map').on('measure:created', this.rectangleMeasured, this);
+    this.get('map').dragging.enable();
+    let editTools = new L.Editable(this.get('map'), { drawingCursor: this.get('cursor') });
+    this.set('_editTools', editTools);
+    this.get('map').on('editable:drawing:end', this.rectangleStarted, this);
+    this._drawRectangle();
   },
 
   disable() {
     this._super(...arguments);
-    var map = this.get('map');
-    map.dragging.disable();
-    this.rectangleClose();
-    map.editTools.stopDrawing();
+    this.get('map').dragging.disable();
+    this.get('map').off('editable:drawing:end', this.rectangleStarted, this);
+    this.get('_editTools').stopDrawing();
   },
+
+  _drawRectangle() {
+    this.get('_editTools').startRectangle();
+  }
 });
