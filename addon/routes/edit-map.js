ed2546c232fc4f24155e8918ecb00f99efe42c17
Serega Matin <seregamatin@gmail.com>
2016-10-27 23:59:28 +0500

    Add CSW connections loading on map routes

--- addon/routes/edit-map.js
+++ addon/routes/edit-map.js
@@ -4,6 +4,7 @@
 
 import EditFormRoute from 'ember-flexberry/routes/edit-form';
 import MapLayersLoaderMixin from '../mixins/map-layers-loader';
+import { Query } from 'ember-flexberry-data';
 
 /**
   Edit map route.
@@ -33,16 +34,79 @@ export default EditFormRoute.extend(MapLayersLoaderMixin, {
   modelName: 'new-platform-flexberry-g-i-s-map',
 
   /**
+    Name of CSW connection model projection to be used as record's properties limitation.
+
+    @property cswConnectionProjection
+    @type String
+    @default 'MapE'
+  */
+  cswConnectionProjection: 'CswConnectionE',
+
+  /**
+    Name of CSW connection model to be used as form's record type.
+
+    @property modelName
+    @type String
+    @default 'new-platform-flexberry-g-i-s-csw-connection'
+  */
+  cswConnectionModelName: 'new-platform-flexberry-g-i-s-csw-connection',
+
+  /**
+    Loaded CSW connections.
+
+    @property cswConnections
+    @type Object[]
+    @default null
+  */
+  cswConnections: null,
+
+  /**
+    Loads available CSW connections.
+
+    @method loadCswConnections
+    @return {<a href="http://emberjs.com/api/classes/RSVP.Promise.html">Ember.RSVP.Promise</a>}
+    Promise which will return available CSW connections after it will be resolved.
+  */
+  loadCswConnections() {
+    let store = this.get('store');
+    let modelName = this.get('cswConnectionModelName');
+    let query = new Query.Builder(store)
+      .from(modelName)
+      .selectByProjection(this.get('cswConnectionProjection'));
+
+    return store.query(modelName, query.build()).then((cswConnections) => {
+      this.set('cswConnections', cswConnections.toArray());
+    });
+  },
+
+  /**
     [Model hook](http://emberjs.com/api/classes/Ember.Route.html#method_model) that returns a map project for current route.
 
     @method model
     @param {Object} params
     @param {Object} transition
-    @return {*} Model of map project for current route.
+    @return {<a href="http://emberjs.com/api/classes/RSVP.Promise.html">Ember.RSVP.Promise</a>}
+    Promise which will return map project related to current route & it's layers hierarchy.
   */
-  model() {
-    let mapPromise = this._super(...arguments);
+  model(params) {
+    let mapPromise = this.loadMapLayers(this._super(...arguments));
 
-    return this.loadMapLayers(mapPromise);
+    return this.loadCswConnections().then(() => {
+      return mapPromise;
+    });
+  },
+
+  /**
+    Setups controller for the current route.
+    [More info](http://emberjs.com/api/classes/Ember.Route.html#method_setupController).
+
+    @method setupController
+    @param {Ember.Controller} controller
+    @param {Object} model
+  */
+  setupController(controller, model) {
+    this._super(...arguments);
+
+    controller.set('cswConnections', this.get('cswConnections'));
   }
 });
