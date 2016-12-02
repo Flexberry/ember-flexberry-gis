1f9fe66d8b37329e141c2c77f38aa10e34f11f31
Serega Matin <seregamatin@gmail.com>
2016-12-02 16:45:59 +0500

    Make path to asssets relative to baseURL

--- addon/initializers/leaflet.js
+++ addon/initializers/leaflet.js
@@ -8,10 +8,11 @@
   @for ApplicationInitializer
   @method leaflet.initialize
   @param {<a href="http://emberjs.com/api/classes/Ember.Application.html">Ember.Application</a>} application Ember application.
+  @param {String} Application's base URL from config/environment.js.
 */
-export function initialize(application) {
+export function initialize(application, baseURL) {
   // Set up leaflet images path (see index.js file where leaflet is imported into application's vendor.js).
-  L.Icon.Default.imagePath = '/assets/images/';
+  L.Icon.Default.imagePath = (baseURL || '/') + 'assets/images/';
 }
 
 export default {
