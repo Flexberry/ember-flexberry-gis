d7c41bc709bdbbc9a1b8e7d8becc1480c6664c70
kuzkok <kuzkok@gmail.com>
2017-02-21 09:44:33 +0000

    Docs for layer-result-list

--- addon/components/layer-result-list.js
+++ addon/components/layer-result-list.js
@@ -1,6 +1,16 @@
+/**
+  @module ember-flexberry-gis
+ */
+
 import Ember from 'ember';
 import layout from '../templates/components/layer-result-list';
 
+/**
+  Component for display array of search\identify results
+
+  @class LayerResultListComponent
+  @extends <a href="http://emberjs.com/api/classes/Ember.Component.html">Ember.Component</a>
+ */
 export default Ember.Component.extend({
 
   /**
