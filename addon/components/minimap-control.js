1f687423c531619ba752d269dfa5c3a8d2062840
avsibiryakov <avsibiryakov@ics.perm.ru>
2017-02-21 11:55:48 +0500

    add documentation comments

--- addon/components/minimap-control.js
+++ addon/components/minimap-control.js
@@ -12,16 +12,34 @@ import layout from '../templates/components/minimap-control';
 */
 export default BaseControl.extend({
 
+  /**
+    Leaflet layerGroup for this control
+    @property layerGroup
+    @type L.LayerGroup
+    @default null
+   */
   layerGroup: null,
 
   leafletOptions: ['toggleDisplay', 'collapsedWidth', 'collapsedHeight'],
 
+  /**
+    Sets whether the minimap should have a button to minimise it.
+   */
   toggleDisplay: true,
 
+  /**
+    The width of the toggle marker and the minimap when collapsed, in pixels
+   */
   collapsedWidth: 18,
 
+  /**
+    The height of the toggle marker and the minimap when collapsed, in pixels.
+   */
   collapsedHeight: 18,
 
+  /**
+    Reference to component's template.
+   */
   layout,
 
   init() {
