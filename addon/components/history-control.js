e25cc4807e893f9513d3c05e5de0b5641efa86da
Vasenina Darya <DVasenina@ics.perm.ru>
2017-04-07 14:44:41 +0100

    cr

--- addon/components/history-control.js
+++ addon/components/history-control.js
@@ -21,9 +21,9 @@ export default BaseControl.extend({
   forwardImage: 'angle right icon',
 
   /**
-    - position - sets which corner of the map to place your controls. 
+    - position - sets which corner of the map to place your controls.
     possible values are 'topleft' | 'topright'(default) | 'bottomleft' | 'bottomright'
-    - maxMovesToSave - the number of moves in the history to save before clearing out the oldest. 
+    - maxMovesToSave - the number of moves in the history to save before clearing out the oldest.
     default value is 10, use 0 or a negative number to make unlimited.
     - backImage, forwardImage - the class used for the button images. defaults are 'fa fa-caret-left' and 'fa fa-caret-right', respectively.
     - no image will be displayed if set to empty string.
@@ -31,9 +31,9 @@ export default BaseControl.extend({
     - backTooltip, forwardTooltip - tooltip contents. defaults are 'Go to Previous Extent' and 'Go to Next Extent', respectively.
     - backImageBeforeText, forwardImageBeforeText - when both text and image are present
     - orientation - 'vertical' | 'horizontal'(default) - whether to position the buttons on top of one another or side-by-side.
-    - useExternalControls - true | false(default) - set to true to hide these controls on the map and instead use your own controls. 
+    - useExternalControls - true | false(default) - set to true to hide these controls on the map and instead use your own controls.
     See the Events and API for more details on this.
-    - shouldSaveMoveInHistory - function(zoomCenter) { return true; } a callback you can provide that gets called with every move. 
+    - shouldSaveMoveInHistory - function(zoomCenter) { return true; } a callback you can provide that gets called with every move.
     return false to not save a move.
   */
   leafletOptions: [
