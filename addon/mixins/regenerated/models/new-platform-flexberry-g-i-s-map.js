b4f0f914734d1fe7a9215da853cd2d573f95455f
almostcake <konstantin.mizev@gmail.com>
2017-05-26 14:28:14 +0500

    Added captions for linkParameters

--- addon/mixins/regenerated/models/new-platform-flexberry-g-i-s-map.js
+++ addon/mixins/regenerated/models/new-platform-flexberry-g-i-s-map.js
@@ -73,11 +73,11 @@ export let defineProjections = function (model) {
 
         }, { hidden: true }),
         linkParameter: Projection.hasMany('new-platform-flexberry-g-i-s-link-parameter', '', {
-          objectField: Projection.attr(''),
-          layerField: Projection.attr(''),
-          expression: Projection.attr(''),
-          queryKey: Projection.attr(''),
-          linkField: Projection.attr('')
+          objectField: Projection.attr('Поле объекта'),
+          layerField: Projection.attr('Поле слоя'),
+          expression: Projection.attr('Выражение'),
+          queryKey: Projection.attr('Параметр запроса'),
+          linkField: Projection.attr('Поле связи')
         }, {
           hidden: true
         })
