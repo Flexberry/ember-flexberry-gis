98d2e9683e48099f76e9609616d8b93bd42a5f56
almostcake <konstantin.mizev@gmail.com>
2017-06-09 17:05:08 +0500

    Added support for custom precision, custom and crs for measure-coordinates maptool

--- addon/map-tools/measure-coordinates.js
+++ addon/map-tools/measure-coordinates.js
@@ -12,6 +12,52 @@ import MeasureMapTool from './measure';
 */
 export default MeasureMapTool.extend({
   /**
+    Tool's coordinate reference system (CRS).
+
+    @property crs
+    @type <a href="http://leafletjs.com/reference-1.0.0.html#crs">L.CRS</a>
+    @default null
+  */
+  crs: null,
+
+  /**
+    Coordinates tool's captions
+
+    @property captions
+    @type {Object}
+    @default null
+    @example ```javascript
+    {
+      northLatitude: ' с.ш. ',
+      southLatitude: ' ю.ш. ',
+      eastLongitude: ' в.д. ',
+      westLongitude: ' з.д. ',
+      x: 'X: ',
+      y: 'Y: '
+    }
+    ```
+  */
+  captions: null,
+
+  /**
+    Coordinates tool's results precision
+
+    @property precision
+    @type Number
+    @default null
+  */
+  precision: null,
+
+  /**
+    Flag: indicates whether to display coordinates instead of LatLng's
+
+    @property displayCoordinates
+    @default false
+    @type Boolean
+  */
+  displayCoordinates: false,
+
+  /**
     Enables tool.
 
     @method _enable
@@ -19,6 +65,16 @@ export default MeasureMapTool.extend({
   */
   _enable() {
     this._super(...arguments);
-    this.get('_measureTools').markerBaseTool.startMeasure();
+    let crs = this.get('crs');
+    let captions = this.get('captions');
+    let precision = this.get('precision');
+    let displayCoordinates = this.get('displayCoordinates');
+
+    this.get('_measureTools').markerBaseTool.startMeasure({
+      crs: crs,
+      precision: precision,
+      captions: captions,
+      displayCoordinates: displayCoordinates
+    });
   }
 });
