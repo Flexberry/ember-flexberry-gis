f433b40f197da1e482b94b6ed710aa686a669162
almostcake <konstantin.mizev@gmail.com>
2017-07-17 10:44:42 +0500

    fixed code structure

--- addon/components/layer-result-list.js
+++ addon/components/layer-result-list.js
@@ -18,7 +18,7 @@ export default Ember.Component.extend({
     @property _noData
     @type boolean
     @default false
-   */
+  */
   _noData: false,
 
   /**
@@ -26,7 +26,7 @@ export default Ember.Component.extend({
     @property _hasError
     @type boolean
     @default false
-   */
+  */
   _hasError: false,
 
   /**
@@ -34,7 +34,7 @@ export default Ember.Component.extend({
     @property _selectedFeature
     @type GeoJSON feature
     @default null
-   */
+  */
   _selectedFeature: null,
 
   classNames: ['layer-result-list'],
@@ -46,7 +46,7 @@ export default Ember.Component.extend({
     @property serviceLayer
     @type L.FeatureGroup
     @default null
-   */
+  */
   serviceLayer: null,
 
   /**
@@ -54,7 +54,7 @@ export default Ember.Component.extend({
     @property leafletMap
     @type L.Map
     @default null
-   */
+  */
   leafletMap: null,
 
   /**
@@ -73,13 +73,103 @@ export default Ember.Component.extend({
     @property results
     @type Ember.A()
     @default null
-   */
+  */
   results: null,
+
+  actions: {
+    /**
+        Handles inner FeatureResultItem's bubbled 'selectFeature' action.
+        Invokes component's {{#crossLink "FeatureResultItem/sendingActions.selectFeature:method"}}'selectFeature'{{/crossLink}} action.
+
+        @method actions.selectFeature
+        @param {Object} feature Describes inner FeatureResultItem's feature object or array of it.
+    */
+    selectFeature(feature) {
+      let selectedFeature = this.get('_selectedFeature');
+      let serviceLayer = this.get('serviceLayer');
+
+      if (selectedFeature !== feature) {
+        if (!Ember.isNone(serviceLayer)) {
+          serviceLayer.clearLayers();
+
+          if (Ember.isArray(feature)) {
+            feature.forEach((item) => this._selectFeature(item));
+          } else {
+            this._selectFeature(feature);
+          }
+        }
+
+        this.set('_selectedFeature', feature);
+      }
+
+      // Send action despite of the fact feature changed or not. User can disable layer anytime.
+      this.sendAction('featureSelected', feature);
+    },
+
+    /**
+      Select passed feature and zoom map to its layer bounds
+      @method actions.zoomTo
+      @param {Object} feature Describes inner FeatureResultItem's feature object or array of it.
+    */
+    zoomTo(feature) {
+      let serviceLayer = this.get('serviceLayer');
+      if (!Ember.isNone(serviceLayer)) {
+        this.send('selectFeature', feature);
+
+        let bounds;
+        if (typeof (serviceLayer.getBounds) === 'function') {
+          bounds = serviceLayer.getBounds();
+        } else {
+          let featureGroup = L.featureGroup(serviceLayer.getLayers());
+          if (featureGroup) {
+            bounds = featureGroup.getBounds();
+          }
+        }
+
+        if (!Ember.isBlank(bounds)) {
+          // 'bound.pad(1)' bounds with zoom decreased by 1 point (padding).
+          //  That allows to make map's bounds slightly larger than serviceLayer's bounds to make better UI.
+          this.get('leafletMap').fitBounds(bounds.pad(1));
+        }
+      }
+    },
+
+    /**
+      Select passed feature and pan map to its layer centroid
+      @method actions.panTo
+      @param {Object} feature Describes inner FeatureResultItem's feature object or array of it.
+    */
+    panTo(feature) {
+      let latLng;
+      if (typeof (feature.leafletLayer.getBounds) === 'function') {
+        latLng = feature.leafletLayer.getBounds().getCenter();
+      } else {
+        latLng = feature.leafletLayer.getLatLng();
+      }
+
+      // TODO: pass action with panTo latLng outside
+      this.get('leafletMap').panTo(latLng);
+      this.send('selectFeature', feature);
+    }
+  },
+
+  /**
+    Set selected feature and add its layer to serviceLayer on map.
+    @method _selectFeature
+    @param {Object} feature Describes inner FeatureResultItem's feature object or array of it.
+    @private
+  */
+  _selectFeature(feature) {
+    let serviceLayer = this.get('serviceLayer');
+    if (!Ember.isNone(feature)) {
+      serviceLayer.addLayer(feature.leafletLayer);
+    }
+  },
 
   /**
     Observer for passed results
     @method _resultObserver
-   */
+  */
   _resultObserver: Ember.observer('results', function () {
     this.send('selectFeature', null);
     this.set('_hasError', false);
@@ -197,97 +287,7 @@ export default Ember.Component.extend({
         this.send('zoomTo', displayResults.objectAt(0).features);
       }
     });
-  }),
-
-  actions: {
-    /**
-        Handles inner FeatureResultItem's bubbled 'selectFeature' action.
-        Invokes component's {{#crossLink "FeatureResultItem/sendingActions.selectFeature:method"}}'selectFeature'{{/crossLink}} action.
-
-        @method actions.selectFeature
-        @param {Object} feature Describes inner FeatureResultItem's feature object or array of it.
-    */
-    selectFeature(feature) {
-      let selectedFeature = this.get('_selectedFeature');
-      let serviceLayer = this.get('serviceLayer');
-
-      if (selectedFeature !== feature) {
-        if (!Ember.isNone(serviceLayer)) {
-          serviceLayer.clearLayers();
-
-          if (Ember.isArray(feature)) {
-            feature.forEach((item) => this._selectFeature(item));
-          } else {
-            this._selectFeature(feature);
-          }
-        }
-
-        this.set('_selectedFeature', feature);
-      }
-
-      // Send action despite of the fact feature changed or not. User can disable layer anytime.
-      this.sendAction('featureSelected', feature);
-    },
-
-    /**
-      Select passed feature and zoom map to its layer bounds
-      @method actions.zoomTo
-      @param {Object} feature Describes inner FeatureResultItem's feature object or array of it.
-     */
-    zoomTo(feature) {
-      let serviceLayer = this.get('serviceLayer');
-      if (!Ember.isNone(serviceLayer)) {
-        this.send('selectFeature', feature);
-
-        let bounds;
-        if (typeof (serviceLayer.getBounds) === 'function') {
-          bounds = serviceLayer.getBounds();
-        } else {
-          let featureGroup = L.featureGroup(serviceLayer.getLayers());
-          if (featureGroup) {
-            bounds = featureGroup.getBounds();
-          }
-        }
-
-        if (!Ember.isBlank(bounds)) {
-          // 'bound.pad(1)' bounds with zoom decreased by 1 point (padding).
-          //  That allows to make map's bounds slightly larger than serviceLayer's bounds to make better UI.
-          this.get('leafletMap').fitBounds(bounds.pad(1));
-        }
-      }
-    },
-
-    /**
-      Select passed feature and pan map to its layer centroid
-      @method actions.panTo
-      @param {Object} feature Describes inner FeatureResultItem's feature object or array of it.
-     */
-    panTo(feature) {
-      let latLng;
-      if (typeof (feature.leafletLayer.getBounds) === 'function') {
-        latLng = feature.leafletLayer.getBounds().getCenter();
-      } else {
-        latLng = feature.leafletLayer.getLatLng();
-      }
-
-      // TODO: pass action with panTo latLng outside
-      this.get('leafletMap').panTo(latLng);
-      this.send('selectFeature', feature);
-    }
-  },
-
-  /**
-    Set selected feature and add its layer to serviceLayer on map.
-    @method _selectFeature
-    @param {Object} feature Describes inner FeatureResultItem's feature object or array of it.
-    @private
-   */
-  _selectFeature(feature) {
-    let serviceLayer = this.get('serviceLayer');
-    if (!Ember.isNone(feature)) {
-      serviceLayer.addLayer(feature.leafletLayer);
-    }
-  }
+  })
 
   /**
     Component's action invoking when feature item was selected.
