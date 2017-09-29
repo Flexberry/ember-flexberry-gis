21dd03b0b1a3b30fb88f9bf75a413927ce048158
kuzkok <kuzkok@gmail.com>
2017-09-29 14:02:34 +0100

    Add documentation comments

--- addon/components/layer-tree-headers/base.js
+++ addon/components/layer-tree-headers/base.js
@@ -1,6 +1,17 @@
+/**
+  @module ember-flexberry-gis
+*/
+
 import Ember from 'ember';
 import DynamicActionsMixin from 'ember-flexberry/mixins/dynamic-actions';
 
+/**
+  Base layer tree header component.
+  Layer components should be inheriting from this.
+
+  @class BaseLayerTreeHeaderComponent
+  @extends <a href="http://emberjs.com/api/classes/Ember.Component.html">Ember.Component</a>
+ */
 export default Ember.Component.extend(DynamicActionsMixin, {
   settingsAsObject: null
 });
