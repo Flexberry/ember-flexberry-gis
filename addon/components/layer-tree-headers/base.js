724393d48d33474e56b3e719a6424ecc2fd13737
kuzkok <kuzkok@gmail.com>
2017-09-29 13:51:55 +0100

    Add logic for layer specific header in tree view

---
+++ addon/components/layer-tree-headers/base.js
@@ -0,0 +1,6 @@
+import Ember from 'ember';
+import DynamicActionsMixin from 'ember-flexberry/mixins/dynamic-actions';
+
+export default Ember.Component.extend(DynamicActionsMixin, {
+  settingsAsObject: null
+});
