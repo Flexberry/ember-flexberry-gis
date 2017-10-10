44c929c1712e683d1e3f157cba82fac497894459
Unknown <deimoss@mail.ru>
2017-10-10 16:19:25 +0500

    Add popup with coordinates. Add locationError alert with message.

--- addon/map-commands/locate.js
+++ addon/map-commands/locate.js
@@ -20,8 +20,13 @@ export default BaseMapCommand.extend({
   */
   _execute(options) {
     this._super(...arguments);
-
     let leafletMap = this.get('leafletMap');
+    leafletMap.once('locationfound',(e)=>{
+        L.marker(e.latlng).addTo(leafletMap).bindPopup('My coordinates: '+ '<br>'+ e.latlng.lat.toFixed(0) +'<br>'+ e.latlng.lng.toFixed(0) ).openPopup();
+    });
+    leafletMap.once('locationerror',(e)=>{
+        alert(e.message);
+    });
     leafletMap.locate({ setView: true, maxZoom: 16 });
   }
 });
