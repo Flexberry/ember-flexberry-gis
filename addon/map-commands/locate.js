f25cd6ef1b4c46be39edf99379430ab6114b214b
Unknown <deimoss@mail.ru>
2017-10-18 14:33:42 +0500

    Code refactoring

--- addon/map-commands/locate.js
+++ addon/map-commands/locate.js
@@ -2,6 +2,7 @@
   @module ember-flexberry-gis
 */
 
+import Ember from 'ember';
 import BaseMapCommand from './base';
 
 /**
@@ -11,10 +12,25 @@ import BaseMapCommand from './base';
   @class LocateMapCommand
   @extends BaseMapCommand
 */
-var locateMarker;
-var locatePopup;
 
 export default BaseMapCommand.extend({
+  /**
+    Location marker variable
+
+    @property locateMarker
+    @type <a href=” http://leafletjs.com/reference-1.2.0.html#marker”>L.Marker</a>
+    @default null
+  */
+  locateMarker: null,
+
+  /**
+    Location marker popup variable
+
+    @property locatePopup
+    @type <a href=” http://leafletjs.com/reference-1.2.0.html#popup”>L.Popup</a>
+    @default null
+  */
+  locatePopup: null,
 
   /**
     Executes map-command.
@@ -25,38 +41,44 @@ export default BaseMapCommand.extend({
   _execute(options) {
     this._super(...arguments);
     let leafletMap = this.get('leafletMap');
+    let locateMarker = this.get('locateMarker');
+    if (Ember.isNone(locateMarker)) {
+      locateMarker = null;
+      this.set('locateMarker', locateMarker);
+    }
 
-    leafletMap.on('locationfound', (e) => {
+    let locatePopup = this.get('locatePopup');
+    if (Ember.isNone(locateMarker)) {
+      locatePopup = null;
+      this.set('locatePopup', locatePopup);
+    }
+
+    leafletMap.once('locationfound', (e) => {
       if (locatePopup != null) {
         leafletMap.closePopup(locatePopup);
       }
 
       locateMarker = L.marker(e.latlng).addTo(leafletMap);
 
-      //jscs:disable maximumLineLength
-      locatePopup = locateMarker.bindPopup(this.get('i18n').t('components.map-commands.locate.lat') + e.latlng.lat.toFixed(0) + '<br>' + this.get('i18n').t('components.map-commands.locate.lng') + e.latlng.lng.toFixed(0)).openPopup();
+      locatePopup = locateMarker.bindPopup(
+        this.get('i18n').t('components.map-commands.locate.lat') + e.latlng.lat.toFixed(0) +
+         '<br>' + this.get('i18n').t('components.map-commands.locate.lng') + e.latlng.lng.toFixed(0)).openPopup();
 
-      //jscs:enable maximumLineLength
-
+      locatePopup.once('popupclose', onPopupClose);
     });
 
-    leafletMap.on('locationerror', (e) => {
-
-      //jscs:disable maximumLineLength
-      L.popup().setLatLng(leafletMap.getCenter()).setContent(this.get('i18n').t('components.map-commands.locate.error') + '<br>' + e.message).openOn(leafletMap);
-
-      //jscs:enable maximumLineLength
-
+    leafletMap.once('locationerror', (e) => {
+      L.popup().setLatLng(
+        leafletMap.getCenter()).setContent(this.get('i18n').t('components.map-commands.locate.error') +
+        '<br>' + e.message).openOn(leafletMap);
     });
 
-    leafletMap.on('popupclose', (e) => {
-      if (locateMarker != null) {
-        leafletMap.removeLayer(locateMarker);
-        locateMarker = null;
-        locatePopup = null;
-      }
+    function onPopupClose() {
+      leafletMap.removeLayer(locateMarker);
+      locateMarker = null;
+      locatePopup = null;
+    }
 
-    });
     leafletMap.locate({ setView: true, maxZoom: 16 });
   }
 });
