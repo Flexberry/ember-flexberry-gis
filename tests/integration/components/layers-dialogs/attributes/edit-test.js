80a140e4826d3638e94f7f78332f1e02df3c9f91
Nik Kataev <nkataev@gmail.com>
2017-10-25 10:20:09 +0500

    Remove unused import

--- tests/integration/components/layers-dialogs/attributes/edit-test.js
+++ tests/integration/components/layers-dialogs/attributes/edit-test.js
@@ -1,5 +1,4 @@
 import { moduleForComponent, test } from 'ember-qunit';
-import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('layers-dialogs/attributes/edit', 'Integration | Component | layers dialogs/attributes/edit', {
   integration: true
