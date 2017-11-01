90db5f79267c0f0cbcc2fc02f5536eff85fff7fa
Nik Kataev <nkataev@gmail.com>
2017-11-01 11:06:05 +0500

    Add test for flexberry-layers-attributes-panel component

--- tests/integration/components/flexberry-layers-attributes-panel-test.js
+++ tests/integration/components/flexberry-layers-attributes-panel-test.js
@@ -1,11 +1,45 @@
+import Ember from 'ember';
 import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('flexberry-layers-attributes-panel', 'Integration | Component | flexberry layers attributes panel', {
   integration: true
 });
 
-test('temporary test', function(assert) {
-  // Set any properties with this.set('myProperty', 'value');
-  // Handle any actions with this.on('myAction', function(val) { ... });
-  assert.equal('', '');
+test('it renders', function(assert) {
+  let geoJsonData = `
+    {
+      "type": "FeatureCollection",
+      "features": [{
+        "type": "Feature",
+        "geometry": {
+          "type": "Point",
+          "coordinates": [102.0, 0.5]
+        },
+        "properties": {
+          "prop0": "value0"
+        }
+      }
+    ]
+  }`;
+  let geoJson = L.geoJSON(JSON.parse(geoJsonData));
+  Ember.set(geoJson, 'readFormat', {
+    featureType: {
+      fieldTypes: { prop0: 'string' },
+      fields: { prop0: (val) => {return val;} }
+    }
+  });
+  this.set('items', Ember.A([{
+    name: 'test layer',
+    leafletObject: geoJson
+  }]));
+
+  this.render(hbs`
+    {{flexberry-layers-attributes-panel items=items folded=false}}
+  `);
+
+  let $tab = this.$().find('div[data-tab="test layer"]');
+
+  assert.equal($tab.length, 1, 'Layer tab was rendered');
+  assert.equal($tab.find('tbody td:last-child').text().trim(), 'value0', 'Property cell was rendered');
 });
