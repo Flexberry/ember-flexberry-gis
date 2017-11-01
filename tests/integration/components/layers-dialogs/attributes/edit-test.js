a75c1dd0cad662acd64168ac2a23ad63d6fe8a0b
Nik Kataev <nkataev@gmail.com>
2017-11-01 09:37:25 +0500

    Add layer attributes edit window test

--- tests/integration/components/layers-dialogs/attributes/edit-test.js
+++ tests/integration/components/layers-dialogs/attributes/edit-test.js
@@ -1,11 +1,38 @@
 import { moduleForComponent, test } from 'ember-qunit';
+import hbs from 'htmlbars-inline-precompile';
 
 moduleForComponent('layers-dialogs/attributes/edit', 'Integration | Component | layers dialogs/attributes/edit', {
   integration: true
 });
 
-test('temporary test', function(assert) {
+test('it renders', function(assert) {
+  assert.expect(2);
+
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
-  assert.equal('', '');
+
+  this.set('data', {
+    field: 'test field'
+  });
+  this.set('fieldTypes', {
+    field: 'string'
+  });
+  this.set('visible', true);
+  this.on('onEditRowDialogApprove', function(data) {
+    assert.equal(data.field, 'test field');
+  });
+
+  this.render(hbs`
+    {{layers-dialogs/attributes/edit
+      visible=visible
+      data=data
+      fieldTypes=fieldTypes
+      approve=(action "onEditRowDialogApprove")
+    }}`);
+
+  // Component is rendered outside the testing container
+  let $window = this.$().closest('#ember-testing-container')
+    .siblings('.ui.modals').find('.flexberry-edit-layer-attributes-dialog');
+  assert.ok($window.find('.flexberry-textbox').length === 1, 'It renders one input');
+  $window.find('.approve.button').click();
 });
