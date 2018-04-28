180705e50b1469a3d6adf763a252225aaed33a7f
Eugene Kolegov <ekolegov@outlook.com>
2018-04-28 12:56:16 +0500

    Fix JSCS errors

--- tests/unit/components/base-vector-layer-test.js
+++ tests/unit/components/base-vector-layer-test.js
@@ -26,29 +26,29 @@ test(`it identify on 'geojson' layer`, function(assert) {
         },
         {
           type: 'Feature',
-          geometry: { type: 'MultiPoint', coordinates: [ [1, 4], [1, 5] ] },
+          geometry: { type: 'MultiPoint', coordinates: [[1, 4], [1, 5]] },
         },
         {
           type: 'Feature',
-          geometry: { type: 'LineString', coordinates: [ [2, 8], [3, 9], [4, 8] ] },
+          geometry: { type: 'LineString', coordinates: [[2, 8], [3, 9], [4, 8]] },
         },
         {
           type: 'Feature',
-          geometry: { type: 'MultiLineString', coordinates: [ [ [6, 8], [6, 9] ], [ [8, 8], [8, 9] ] ] },
+          geometry: { type: 'MultiLineString', coordinates: [[[6, 8], [6, 9]], [[8, 8], [8, 9]]] },
         },
         {
           type: 'Feature',
-          geometry: { type: 'Polygon', coordinates: [ [ [5, 5], [5, 6], [6, 6], [6, 5] ] ] },
+          geometry: { type: 'Polygon', coordinates: [[[5, 5], [5, 6], [6, 6], [6, 5]]] },
         },
         {
           type: 'Feature',
-          geometry: { type: 'MultiPolygon', coordinates: [ [ [ [7, 1], [8, 1], [8, 2] ] ], [ [ [6, 2], [6, 3], [7, 3] ] ] ] },
+          geometry: { type: 'MultiPolygon', coordinates: [[[[7, 1], [8, 1], [8, 2]]], [[[6, 2], [6, 3], [7, 3]]]] },
         },
         {
           type: 'GeometryCollection',
           geometries: [
-            { type: 'Polygon', coordinates: [ [ [3, 3], [4, 3], [4, 2] ] ] },
-            { type: 'LineString', coordinates: [ [1, 1], [2, 1], [2, 2] ] },
+            { type: 'Polygon', coordinates: [[[3, 3], [4, 3], [4, 2]]] },
+            { type: 'LineString', coordinates: [[1, 1], [2, 1], [2, 2]] },
           ],
         },
         {
@@ -75,15 +75,15 @@ test(`it identify on 'geojson' layer`, function(assert) {
 
   assert.expect(3);
   let done = assert.async(3);
-  component.identify(select([ [4, 4], [2, 4], [2, 6], [4, 6] ])).then((results) => {
+  component.identify(select([[4, 4], [2, 4], [2, 6], [4, 6]])).then((results) => {
     assert.equal(results.length, 0, 'Empty area is selected.');
   }).finally(done);
 
-  component.identify(select([ [9, 5], [8, 8], [6, 6] ])).then((results) => {
+  component.identify(select([[9, 5], [8, 8], [6, 6]])).then((results) => {
     assert.equal(results.length, 3, 'Point (from FeatureCollection), MultiLineString and Polygon.');
   }).finally(done);
 
-  component.identify(select([ [9, 1], [1, 1], [1, 9], [9, 9] ])).then((results) => {
+  component.identify(select([[9, 1], [1, 1], [1, 9], [9, 9]])).then((results) => {
     assert.equal(results.length, 9, 'All geometries is selected.');
   }).finally(done);
 });
