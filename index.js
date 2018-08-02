ad64410dc59c998fcb6b147c230c89429a46f720
Abramova Snezhana <sabramova@ics.perm.ru>
2018-08-02 14:31:27 +0500

    intermediate variant2

--- index.js
+++ index.js
@@ -124,9 +124,5 @@ module.exports = {
     app.import(app.bowerDirectory + '/leaflet.markercluster/dist/leaflet.markercluster.js');
     app.import(app.bowerDirectory + '/leaflet.markercluster/dist/MarkerCluster.css');
     app.import(app.bowerDirectory + '/leaflet.markercluster/dist/MarkerCluster.Default.css');
-
-    // Leaflet.textpath Extensions.
-    app.import('vendor/leaflet/utils/ctxtextpath.js');
-
   }
 };
