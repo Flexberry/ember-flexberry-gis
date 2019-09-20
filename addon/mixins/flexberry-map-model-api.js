a5bc15f76b4b48da5fea0a8265f78affad3a9df3
Rasin Jean <rasin@ics.perm.ru>
2019-09-20 16:28:27 +0500

    Start work

--- addon/mixins/flexberry-map-model-api.js
+++ addon/mixins/flexberry-map-model-api.js
@@ -19,6 +19,39 @@ export default Ember.Mixin.create({
     this._setVisibility(layerIds);
   },
 
+  /**
+    Remove shape from layer.
+
+    @method deleteLayerObject.
+    @param {String} id layer.
+    @param {String} id shape.
+  */
+  deleteLayerObject(layerId, objectId) {//57cebb75-a9a1-47b0-877a-49fdccb86ad4 ,
+    debugger;
+    const layers = this.get('mapLayer');
+    const layer = layers.findBy('id', layerId);
+
+    // layerIds.forEach(id => {
+    //   const layer = layers.findBy('id', id);
+    //   if (layer) {
+    //     //layer.set('visibility', visibility);
+    //   }
+    // });
+  },
+
+  /**
+    Remove shapes from layer.
+
+    @method deleteLayerObjects.
+    @param {String} id layer.
+    @param {Array} array of id shapes.
+  */
+  deleteLayerObjects(layerId, objectIds) {
+    objectIds.forEach(id => {
+      deleteLayerObject(layerId, id)
+    });
+  },
+
   _setVisibility(layerIds, visibility = false) {
     if (Ember.isArray(layerIds)) {
       const layers = this.get('mapLayer');
