42c33e0ba9bb17a6b4a91738fb5a9e266ba5bde4
Арсений Темник <arsenitem@bk.ru>
2020-01-13 18:30:53 +0500

    added locales+styles

---
+++ addon/locales/en/components/compare-object-geometries-panel.js
@@ -0,0 +1,15 @@
+export default {
+  'add-to-fav-caption': 'Add to favorite',
+  'compare-objects-caption': 'Compare objects',
+  'comparison-objects-caption': 'Comparing objects',
+  'object-info-caption': 'Information about objects',
+  'object-area': 'Area:',
+  'm2': 'м²',
+  'm': 'м',
+  'distance-between': 'Distance between objects:',
+  'intersection': 'Intersected area',
+  'non-intersection': 'Non-intersected area',
+  'cords': 'Координаты',
+  'x':'X',
+  'y':'Y'
+};
