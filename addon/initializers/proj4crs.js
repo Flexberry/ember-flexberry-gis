166624f564197452eb0887fb7eba0959796767b4
Арсений Темник <arsenitem@bk.ru>
2020-02-20 11:56:30 +0500

    added jshint ignore

--- addon/initializers/proj4crs.js
+++ addon/initializers/proj4crs.js
@@ -3,13 +3,14 @@
 */
 
 /**
-  Define custom proections forproj4.
+  Define custom projections for proj4.
 
   @for ApplicationInitializer
   @method proj4crs.initialize
   @param {<a href="http://emberjs.com/api/classes/Ember.Application.html">Ember.Application</a>} application Ember application.
 */
 export function initialize(application) {
+  /* jshint ignore:start */
   proj4.defs([
     [
       'EPSG:32640',
@@ -29,6 +30,7 @@ export function initialize(application) {
       '+proj=tmerc +lat_0=0 +lon_0=59.55 +k=1 +x_0=3250000 +y_0=-5914743.504 +ellps=krass +units=m +no_defs +towgs84=23.57,-140.95,-79.8,0.0,0.35,0.79,0.22'
     ]
   ]);
+  /* jshint ignore:end */
 }
 
 export default {
