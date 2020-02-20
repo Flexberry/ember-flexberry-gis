44f625115f485cd9a60aa0c22f0ddc73861dc0ae
Арсений Темник <arsenitem@bk.ru>
2020-02-20 10:04:08 +0500

    decorative changes

--- addon/mixins/flexberry-map-model-api.js
+++ addon/mixins/flexberry-map-model-api.js
@@ -292,7 +292,7 @@ export default Ember.Mixin.create({
           return helpers.point([object._latlng.lat, object._latlng.lng]);
         } else {
           let latlngs = object.getBounds().getCenter();
-          return helpers.point( [latlngs.lat, latlngs.lng]);
+          return helpers.point([latlngs.lat, latlngs.lng]);
         }
       };
       const firstObject =  getObjectCenterPoint.call(this, objA);
@@ -852,26 +852,6 @@ export default Ember.Mixin.create({
     @private
   */
   _convertObjectCoordinates(object, crsName = null) {
-    //Что с этим делать? куда перенести?
-    // proj4.defs([
-    //   [
-    //     'EPSG:32640',
-    //     '+proj=utm +zone=40 +datum=WGS84 +units=m +no_defs'
-    //   ],
-    //   [
-    //     'EPSG:59001',
-    //     '+proj=tmerc +lat_0=0 +lon_0=53.55 +k=1 +x_0=1250000 +y_0=-5914743.504 +ellps=krass +units=m +no_defs +towgs84=23.57,-140.95,-79.8,0.0,0.35,0.79,0.22'
-    //   ],
-    //   [
-    //     'EPSG:59002',
-    //     '+proj=tmerc +lat_0=0 +lon_0=56.55 +k=1 +x_0=2250000 +y_0=-5914743.504 +ellps=krass +units=m +no_defs'+
-    //     ' +towgs84=23.57,-140.95,-79.8,0.0,0.35,0.79,-0.22'
-    //   ],
-    //   [
-    //     'EPSG:59003',
-    //     '+proj=tmerc +lat_0=0 +lon_0=59.55 +k=1 +x_0=3250000 +y_0=-5914743.504 +ellps=krass +units=m +no_defs +towgs84=23.57,-140.95,-79.8,0.0,0.35,0.79,0.22'
-    //   ]
-    // ]);
     let firstProjection = object.options.crs.code;
     let baseProjection = crsName ? crsName : 'EPSG:4326';
     if (firstProjection !== baseProjection) {
@@ -901,6 +881,7 @@ export default Ember.Mixin.create({
       return object;
     }
   },
+
   /**
     Get coordinates point.
     @method getCoordPoint
