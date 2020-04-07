5fb4c88c20a1368d586423de7493dc08095564c3
sabramova <sabramova@ICS-KK16225>
2020-04-07 10:43:17 +0500

    fix layerProperties

--- addon/components/layers-dialogs/add.js
+++ addon/components/layers-dialogs/add.js
@@ -142,9 +142,9 @@ let FlexberryAddLayerDialogComponent = FlexberryEditLayerDialogComponent.extend(
     onApprove() {
       let file = this.get('_fileControl');
       if (!Ember.isNone(file)) {
-        let layerName = JSON.parse(this.get('getLayerProperties')().settings).layers.split(':');
-        let url = JSON.parse(this.get('getLayerProperties')().settings).url.split('/geoserver');
         let layerProperties = this.get('getLayerProperties')();
+        let layerName = JSON.parse(layerProperties.settings).layers.split(':');
+        let url = JSON.parse(layerProperties.settings).url.split('/geoserver');
         if (!Ember.isNone(layerName)) {
           let _this = this;
           let config = Ember.getOwner(this).resolveRegistration('config:environment');
