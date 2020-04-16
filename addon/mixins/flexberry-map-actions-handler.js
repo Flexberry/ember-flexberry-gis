930d9eace35f6d06a40a772ef21946f69b4cf6de
arsenitem <arsenitem@bk.ru>
2020-04-16 13:05:08 +0500

    no bringToFront for tiles

--- addon/mixins/flexberry-map-actions-handler.js
+++ addon/mixins/flexberry-map-actions-handler.js
@@ -3,6 +3,7 @@
 */
 
 import Ember from 'ember';
+import TileLayer from '../layers/tile';
 
 /**
   Mixin containing handlers for
@@ -62,9 +63,13 @@ export default Ember.Mixin.create({
         let hierarchy = this.get('model.hierarchy').sortBy('index');
         hierarchy.forEach((layer) => {
           let leafletObject = layer.get('_leafletObject');
-          if ((leafletObject != null) && (leafletObject.bringToFront instanceof Function) && layer.get('visibility')) {
+          if ((leafletObject != null) && (leafletObject.bringToFront instanceof Function) && layer.get('visibility') ) {
             try {
-              leafletObject.bringToFront();
+              let className = Ember.get(layer, 'type');
+              let layerType = Ember.getOwner(this).knownForType('layer', className);
+              if (!(layerType instanceof TileLayer)) {
+                leafletObject.bringToFront();
+              }
             } catch (e) {
               //Terrible trouble, we need to figure it out
             }
