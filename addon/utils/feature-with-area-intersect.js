4e8d39f69f864a23c890949fb08394c6d6011afa
sabramova <sabramova@ICS-KK16225>
2020-07-16 17:57:25 +0500

    intesectArea only polygon

--- addon/utils/feature-with-area-intersect.js
+++ addon/utils/feature-with-area-intersect.js
@@ -3,20 +3,22 @@ import jsts from 'npm:jsts';
 import area from 'npm:@turf/area';
 
 export default function featureWithAreaIntersect(featureA, geoLayer, leafletLayer, mapModel) {
-  let objB = geoLayer;
-  let featureB = leafletLayer.options.crs.code === 'EPSG:4326' ? objB : mapModel._convertObjectCoordinates(leafletLayer.options.crs.code, objB);
-  let geojsonReader = new jsts.io.GeoJSONReader();
-  let objAJsts = geojsonReader.read(featureA.geometry);
-  let objBJsts = geojsonReader.read(featureB.geometry);
-  let intersected = objAJsts.intersection(objBJsts);
-  let geojsonWriter = new jsts.io.GeoJSONWriter();
-  let intersectionRes = geojsonWriter.write(intersected);
-  if (intersectionRes) {
-    let intesectArea = area(intersectionRes);
-    if (!Ember.isNone(geoLayer.properties)) {
-      geoLayer.properties.intesectionArea = intesectArea;
-    } else {
-      geoLayer.properties = { intesectionArea: intesectArea };
+  if (leafletLayer instanceof L.Polygon) {
+    let objB = geoLayer;
+    let featureB = leafletLayer.options.crs.code === 'EPSG:4326' ? objB : mapModel._convertObjectCoordinates(leafletLayer.options.crs.code, objB);
+    let geojsonReader = new jsts.io.GeoJSONReader();
+    let objAJsts = geojsonReader.read(featureA.geometry);
+    let objBJsts = geojsonReader.read(featureB.geometry);
+    let intersected = objAJsts.intersection(objBJsts);
+    let geojsonWriter = new jsts.io.GeoJSONWriter();
+    let intersectionRes = geojsonWriter.write(intersected);
+    if (intersectionRes) {
+      let intesectArea = area(intersectionRes);
+      if (!Ember.isNone(geoLayer.properties)) {
+        geoLayer.properties.intesectionArea = intesectArea;
+      } else {
+        geoLayer.properties = { intesectionArea: intesectArea };
+      }
     }
   }
 
