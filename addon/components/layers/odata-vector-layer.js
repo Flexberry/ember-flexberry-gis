313203676a3c9fd778ef59f80dd32b258fe7fe24
sabramova <sabramova@ICS-KK16225>
2020-07-27 21:54:49 +0500

    fix error delete and add callback for save

--- addon/components/layers/odata-vector-layer.js
+++ addon/components/layers/odata-vector-layer.js
@@ -78,7 +78,7 @@ export default BaseVectorLayer.extend({
 
         models.forEach(model => {
           let ids = insertedIds.filter(id => {
-            return model.get('id') === id;
+            return Ember.isNone(model) ? false : model.get('id') === id;
           });
           if (ids.length > 0) {
             insertedModelId.push(ids[0]);
@@ -102,6 +102,8 @@ export default BaseVectorLayer.extend({
         console.log('Error save: ' + e);
         leafletObject.fire('save:failed', e);
       });
+    } else {
+      leafletObject.fire('save:success', { layers: [] });
     }
 
     return leafletObject;
