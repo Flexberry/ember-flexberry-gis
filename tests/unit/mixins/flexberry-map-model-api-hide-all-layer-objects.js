cf86086e19f453f5d414bb40261187b5354c5cfb
Роман Капканников <kapkannikov@ics.perm.ru>
2020-08-17 17:20:44 +0500

    Добавил тест на метод hideAllLayerObjects

---
+++ tests/unit/mixins/flexberry-map-model-api-hide-all-layer-objects.js
@@ -0,0 +1,40 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import FlexberryMapModelApiMixin from 'ember-flexberry-gis/mixins/flexberry-map-model-api';
+import sinon from 'sinon';
+import OdataVector from 'ember-flexberry-gis/layers/odata-vector';
+
+module('Unit | Mixin | test method');
+
+let mapApiMixinObject = Ember.Object.extend(FlexberryMapModelApiMixin);
+
+test('test method hideAllLayerObjects continueLoading - true', function(assert) {
+  //Arrange
+  let map = L.map(document.createElement('div'), {
+    center: [51.505, -0.09],
+    zoom: 13
+  });
+  let labelsLayer = L.featureGroup().addTo(map);
+  labelsLayer.addLayer(L.marker([1,1]));
+  let leafletObject = L.featureGroup().addTo(map);
+  leafletObject.addLayer(L.polygon([[1, 1]]));
+  leafletObject._labelsLayer = labelsLayer;
+  let subject = mapApiMixinObject.create({
+    mapLayer: {
+      findBy() { return Ember.A({ _leafletObject: leafletObject, settingsAsObject: { labelSettings: { signMapObjects: true } } }); }
+    },
+    _getTypeLayer() { return new OdataVector(); },
+    mapApi: { getFromApi() { return map; } }
+  });
+  let spyHasLayer = sinon.spy(map, 'hasLayer');
+  let spyRemoveLayer = sinon.spy(map, 'removeLayer');
+
+  //Act
+  subject.hideAllLayerObjects();
+
+  //Assert
+  assert.equal(spyHasLayer.callCount, 2);
+  assert.equal(spyRemoveLayer.callCount, 3);
+  spyHasLayer.restore();
+  spyRemoveLayer.restore();
+});
