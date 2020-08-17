dc04ac45d3f0059c3e34ae76f4ab7da97d242776
Роман Капканников <kapkannikov@ics.perm.ru>
2020-08-17 09:42:27 +0500

    Исправил табуляцию

--- tests/unit/mixins/flexberry-map-model-api-get-rhumb-test.js
+++ tests/unit/mixins/flexberry-map-model-api-get-rhumb-test.js
@@ -7,109 +7,109 @@ module('Unit | Mixin | method get rhumb');
 let mapApiMixinObject = Ember.Object.extend(FlexberryMapModelApiMixin);
 
 test('test method getRhumb for LineString', function (assert) {
-    //Arrange
-    let done = assert.async(1);
-    let testPolygon = L.polygon([[-41, -111.04], [45, -111.04], [45, -104.05], [41, -104.05]]);
-    let subject = mapApiMixinObject.create({
-      _getModelLayerFeature(layerId, objectId) {
-        return Ember.RSVP.Promise.resolve([null, null, [testPolygon]]);
-      }
-    });
-    const resObj = {
-      type: 'LineString',
-      startPoint: testPolygon._latlngs[0][0],
-      crs: 'EPSG:4326',
-      skip: 1,
-      rhumbCoordinates:
-      [
-          {
-            rhumb: 'СЗ',
-            angle: 0,
-            distance: 9562776.900083829
-          },
-          {
-            rhumb: 'СВ',
-            angle: 90,
-            distance: 549601.2989213171
-          },
-          {
-            rhumb: 'ЮВ',
-            angle: 0,
-            distance: 444780.32093413227
-          },
-          {
-            rhumb: 'ЮЗ',
-            angle: 4.438440688327262,
-            distance: 9145423.193341617
-          }
-      ],
-      coordinates: testPolygon._latlngs
-    };
-  
-    //Act
-    let promise = subject.getRhumb('', '');
-  
-    //Assert
-    assert.ok(promise instanceof Ember.RSVP.Promise);
-    promise.then(
-      (t) => {
-        assert.deepEqual(t, resObj);
-        done();
+  //Arrange
+  let done = assert.async(1);
+  let testPolygon = L.polygon([[-41, -111.04], [45, -111.04], [45, -104.05], [41, -104.05]]);
+  let subject = mapApiMixinObject.create({
+    _getModelLayerFeature(layerId, objectId) {
+      return Ember.RSVP.Promise.resolve([null, null, [testPolygon]]);
+    }
+  });
+  const resObj = {
+    type: 'LineString',
+    startPoint: testPolygon._latlngs[0][0],
+    crs: 'EPSG:4326',
+    skip: 1,
+    rhumbCoordinates:
+    [
+        {
+          rhumb: 'СЗ',
+          angle: 0,
+          distance: 9562776.900083829
+        },
+        {
+          rhumb: 'СВ',
+          angle: 90,
+          distance: 549601.2989213171
+        },
+        {
+          rhumb: 'ЮВ',
+          angle: 0,
+          distance: 444780.32093413227
+        },
+        {
+          rhumb: 'ЮЗ',
+          angle: 4.438440688327262,
+          distance: 9145423.193341617
+        }
+    ],
+    coordinates: testPolygon._latlngs
+  };
+
+  //Act
+  let promise = subject.getRhumb('', '');
+
+  //Assert
+  assert.ok(promise instanceof Ember.RSVP.Promise);
+  promise.then(
+    (t) => {
+      assert.deepEqual(t, resObj);
+      done();
     });
 });
-  
+
 test('test method getRhumb for Polygon', function (assert) {
-    //Arrange
-    let done = assert.async(1);
-    let testPolygon = L.polygon([
-      [
-        [[-41, -111.04], [45, -111.04], [45, -104.05], [41, -104.05]]
-      ]
-    ]);
-    let subject = mapApiMixinObject.create({
-      _getModelLayerFeature(layerId, objectId) {
-        return Ember.RSVP.Promise.resolve([null, null, [testPolygon]]);
-      }
-    });
-    const resObj = {
-      type: 'Polygon',
-      startPoint: testPolygon._latlngs[0][0][0],
-      crs: 'EPSG:4326',
-      skip: 1,
-      rhumbCoordinates:
-      [
-          {
-            rhumb: 'СЗ',
-            angle: 0,
-            distance: 9562776.900083829
-          },
-          {
-            rhumb: 'СВ',
-            angle: 90,
-            distance: 549601.2989213171
-          },
-          {
-            rhumb: 'ЮВ',
-            angle: 0,
-            distance: 444780.32093413227
-          },
-          {
-            rhumb: 'ЮЗ',
-            angle: 4.438440688327262,
-            distance: 9145423.193341617
-          }
-      ],
-      coordinates: testPolygon._latlngs
-    };
-  
-    //Act
-    let promise = subject.getRhumb('', '');
-  
-    //Assert
-    assert.ok(promise instanceof Ember.RSVP.Promise);
-    promise.then(
-      (t) => {
-        assert.deepEqual(t, resObj);
-        done();
+  //Arrange
+  let done = assert.async(1);
+  let testPolygon = L.polygon([
+    [
+      [[-41, -111.04], [45, -111.04], [45, -104.05], [41, -104.05]]
+    ]
+  ]);
+  let subject = mapApiMixinObject.create({
+    _getModelLayerFeature(layerId, objectId) {
+      return Ember.RSVP.Promise.resolve([null, null, [testPolygon]]);
+    }
+  });
+  const resObj = {
+    type: 'Polygon',
+    startPoint: testPolygon._latlngs[0][0][0],
+    crs: 'EPSG:4326',
+    skip: 1,
+    rhumbCoordinates:
+    [
+        {
+          rhumb: 'СЗ',
+          angle: 0,
+          distance: 9562776.900083829
+        },
+        {
+          rhumb: 'СВ',
+          angle: 90,
+          distance: 549601.2989213171
+        },
+        {
+          rhumb: 'ЮВ',
+          angle: 0,
+          distance: 444780.32093413227
+        },
+        {
+          rhumb: 'ЮЗ',
+          angle: 4.438440688327262,
+          distance: 9145423.193341617
+        }
+    ],
+    coordinates: testPolygon._latlngs
+  };
+
+  //Act
+  let promise = subject.getRhumb('', '');
+
+  //Assert
+  assert.ok(promise instanceof Ember.RSVP.Promise);
+  promise.then(
+    (t) => {
+      assert.deepEqual(t, resObj);
+      done();
     });
 });
