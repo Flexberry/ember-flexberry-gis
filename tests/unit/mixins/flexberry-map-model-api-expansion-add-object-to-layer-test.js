9afa355c0b56064b3586654d85341dacdf51d152
Alex <ma3ca3@gmail.com>
2020-08-24 15:38:15 +0500

    added test from method addObjectToLayer

---
+++ tests/unit/mixins/flexberry-map-model-api-expansion-add-object-to-layer-test.js
@@ -0,0 +1,80 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import FlexberryMapModelApiMixin from 'ember-flexberry-gis/mixins/flexberry-map-model-api-expansion';
+import sinon from 'sinon';
+import crsFactory4326 from 'ember-flexberry-gis/coordinate-reference-systems/epsg-4326';
+import crsFactory3395 from 'ember-flexberry-gis/coordinate-reference-systems/epsg-3395';
+
+module('Unit | Mixin | test method addObjectToLayer');
+
+let mapApiMixinObject = Ember.Object.extend(FlexberryMapModelApiMixin);
+let geoJsonObject = L.polygon([[0, 100], [0, 101], [1, 101], [1, 100]]).toGeoJSON();
+
+test('test method addObjectToLayer with EPSG:4326', function(assert) {
+  //Arrange
+  let ownerStub = sinon.stub(Ember, 'getOwner');
+  ownerStub.returns({
+    knownForType() {
+      return {
+        'epsg4326': crsFactory4326,
+        'epsg3395': crsFactory3395,
+      };
+    }
+  });
+  let leafletObject = L.featureGroup();
+  leafletObject.options = { crs: { code: 'EPSG:4326' } };
+  let subject = mapApiMixinObject.create({
+    _getModelLeafletObject() {
+      return [{ id: 1 }, leafletObject];
+    }
+  });
+
+  //Act
+  let result = subject.addObjectToLayer('1', geoJsonObject);
+
+  //Assert
+  assert.equal(leafletObject.getLayers().length, 1);
+  assert.equal(result.layerId, '1');
+  assert.deepEqual(result._latlngs, [[L.latLng(0, 100), L.latLng(0, 101), L.latLng(1, 101), L.latLng(1, 100)]]);
+  ownerStub.restore();
+});
+
+test('test method addObjectToLayer with EPSG:3395', function(assert) {
+  //Arrange
+  let ownerStub = sinon.stub(Ember, 'getOwner');
+  ownerStub.returns({
+    knownForType() {
+      return {
+        'epsg4326': crsFactory4326,
+        'epsg3395': crsFactory3395,
+      };
+    },
+    knownNamesForType() {
+      return ['epsg4326', 'epsg3395'];
+    }
+  });
+  let leafletObject = L.featureGroup();
+  leafletObject.options = { crs: { code: 'EPSG:4326' } };
+  let subject = mapApiMixinObject.create({
+    _getModelLeafletObject() {
+      return [{ id: 1 }, leafletObject];
+    }
+  });
+
+  //Act
+  let result = subject.addObjectToLayer('1', geoJsonObject, 'EPSG:3395');
+
+  //Assert
+  assert.equal(leafletObject.getLayers().length, 1);
+  assert.equal(result.layerId, '1');
+  assert.deepEqual(result._latlngs,
+    [
+      [
+        L.latLng(0, 0.0008983152841195215),
+        L.latLng(0, 0.0009072984369607167),
+        L.latLng(0.00000904328947124462, 0.0009072984369607167),
+        L.latLng(0.00000904328947124462, 0.0008983152841195215)
+      ]
+    ]);
+  ownerStub.restore();
+});
