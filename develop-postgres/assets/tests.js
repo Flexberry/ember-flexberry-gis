5282e73ff917e29a9d62e42ffa88b86eb1127e94
Flexberry-man <mail@flexberry.net>
2020-08-31 07:33:46 +0000

    Update gh-pages for develop-postgres branch

--- develop-postgres/assets/tests.js
+++ develop-postgres/assets/tests.js
@@ -3751,6 +3751,50 @@ define('dummy/tests/unit/components/layers/odata-vector-layer-test', ['exports',
       done();
     });
   });
+
+  (0, _emberQunit.test)('getLayerFeatures() with featureIds=null', function (assert) {
+    var _this3 = this;
+
+    assert.expect(2);
+    var done = assert.async(2);
+    _ember['default'].run(function () {
+      var store = app.__container__.lookup('service:store');
+      store.createRecord('new-platform-flexberry-g-i-s-map-layer');
+      _ember['default'].$.extend(param, {
+        'modelName': 'new-platform-flexberry-g-i-s-map-layer',
+        'projectionName': 'MapLayerL',
+        'geometryField': 'geometryField',
+        'store': store });
+
+      var component = _this3.subject(param);
+
+      var getCountFeaturesStub = _sinon['default'].stub(component, 'getCountFeatures');
+      getCountFeaturesStub.returns(_ember['default'].RSVP.resolve(123));
+
+      var e = {
+        featureIds: null,
+        layer: 'f34ea73d-9f00-4f02-b02d-675d459c972b',
+        results: _ember['default'].A()
+      };
+
+      var leafletLayerPromiseResolved = assert.async();
+      component.get('_leafletLayerPromise').then(function (leafletLayer) {
+        var _leafletObject = L.featureGroup();
+        _leafletObject.options.showExisting = false;
+        component.set('_leafletObject', _leafletObject);
+
+        component.getLayerFeatures(e).then(function (layers) {
+          assert.ok(layers, 'Load with null featureIds');
+          done();
+        });
+      })['finally'](function () {
+        leafletLayerPromiseResolved();
+      });
+
+      assert.ok(component, 'Create odata-layer');
+      done();
+    });
+  });
 });
 define('dummy/tests/unit/components/layers/odata-vector-layer-test.jscs-test', ['exports'], function (exports) {
   'use strict';
@@ -5246,6 +5290,296 @@ define('dummy/tests/unit/mixins/leaflet-properties-test.jshint', ['exports'], fu
   QUnit.test('should pass jshint', function (assert) {
     assert.expect(1);
     assert.ok(true, 'unit/mixins/leaflet-properties-test.js should pass jshint.');
+  });
+});
+define('dummy/tests/unit/mixins/map-model-api-comparelayers-test', ['exports', 'ember', 'ember-flexberry-gis/mixins/flexberry-map-model-api', 'qunit', 'npm:clipper-lib'], function (exports, _ember, _emberFlexberryGisMixinsFlexberryMapModelApi, _qunit, _npmClipperLib) {
+
+  (0, _qunit.module)('Unit | Mixin | map model api comparelayers');
+
+  var mapApiMixinObject = _ember['default'].Object.extend(_emberFlexberryGisMixinsFlexberryMapModelApi['default']);
+
+  var feature1 = {
+    type: 'MultiPolygon',
+    properties: {},
+    coordinates: [[[[3, 3], [3, 7], [8, 7], [8, 3], [3, 3]]]]
+  };
+
+  var feature2 = {
+    type: 'MultiPolygon',
+    properties: {},
+    coordinates: [[[[6, 5], [6, 9], [10, 9], [10, 5], [6, 5]]]]
+  };
+
+  var feature3 = {
+    type: 'MultiPolygon',
+    properties: {},
+    coordinates: [[[[4, 4], [4, 5], [5, 5], [5, 4], [4, 4]]]]
+  };
+
+  var feature4 = {
+    type: 'MultiPolygon',
+    properties: {},
+    coordinates: [[[[10, 3], [9, 3], [9, 2], [10, 2], [10, 3]]]]
+  };
+
+  var crsFactory32640 = {
+    code: 'EPSG:32640',
+    definition: '+proj=utm +zone=40 +datum=WGS84 +units=m +no_defs',
+    create: function create() {
+      var crs = L.extend({}, new L.Proj.CRS(this.code, this.definition), {
+        scale: function scale(zoom) {
+          return 256 * Math.pow(2, zoom);
+        },
+        zoom: function zoom(scale) {
+          return Math.log(scale / 256) / Math.LN2;
+        }
+      });
+      return crs;
+    }
+  };
+  var crs32640 = crsFactory32640.create();
+
+  var geoJson32640 = {
+    type: 'MultiPolygon',
+    properties: {},
+    coordinates: [[[[514059.321485393, 6507392.17766284], [513865.509562311, 6507418.6567982], [513839.790201802, 6507279.05179395], [514059.321485393, 6507392.17766284]]]]
+  };
+
+  (0, _qunit.test)('_coordsToPoints should return array of points to the power of 8', function (assert) {
+    assert.expect(1);
+
+    var featureLayer = L.geoJSON(geoJson32640).getLayers()[0].getLatLngs();
+
+    var subject = mapApiMixinObject.create();
+    var result = subject._coordsToPoints(featureLayer);
+
+    assert.deepEqual(result, [[[{ 'X': 51405932148539, 'Y': 650739217766284 }, { 'X': 51386550956231, 'Y': 650741865679820 }, { 'X': 51383979020180, 'Y': 650727905179395 }]]]);
+  });
+
+  (0, _qunit.test)('_pointsToCoords should return array of coordinates', function (assert) {
+    assert.expect(1);
+
+    var arr = [[[new _npmClipperLib['default'].IntPoint(51405932148539, 650739217766284), new _npmClipperLib['default'].IntPoint(51386550956231, 650741865679820), new _npmClipperLib['default'].IntPoint(51383979020180, 650727905179395)]]];
+
+    var subject = mapApiMixinObject.create();
+    var result = subject._pointsToCoords(arr);
+
+    assert.deepEqual(result, [[[[514059.32148539, 6507392.17766284], [513865.50956231, 6507418.65679820], [513839.79020180, 6507279.05179395], [514059.32148539, 6507392.17766284]]]]);
+  });
+
+  (0, _qunit.test)('_addToArrayPointsAndFeature should return points and features', function (assert) {
+    assert.expect(2);
+    var done = assert.async(1);
+
+    var featureLayer = L.geoJSON(geoJson32640).getLayers()[0];
+    var subject = mapApiMixinObject.create({
+      _getModelLayerFeature: function _getModelLayerFeature() {
+        return _ember['default'].RSVP.resolve(new _ember['default'].RSVP.resolve([null, { options: { crs: crs32640 } }, [featureLayer]]));
+      }
+    });
+
+    var result = subject._addToArrayPointsAndFeature();
+
+    result.then(function (arrayPointsAndFeature) {
+      assert.deepEqual(arrayPointsAndFeature.arrPoints[0], [[[{ 'X': 51405932148539, 'Y': 650739217766284 }, { 'X': 51386550956231, 'Y': 650741865679820 }, { 'X': 51383979020180, 'Y': 650727905179395 }]]]);
+      assert.ok(arrayPointsAndFeature.features[0]);
+      done();
+    });
+  });
+
+  (0, _qunit.test)('differenceLayers should return the difference of layers', function (assert) {
+    assert.expect(4);
+    var done = assert.async(1);
+
+    var featureLayer1 = L.geoJSON(feature1).getLayers()[0];
+    var featureLayer2 = L.geoJSON(feature2).getLayers()[0];
+
+    var subject = mapApiMixinObject.create({
+      _getModelLayerFeature: function _getModelLayerFeature(layer) {
+        if (layer === '1') {
+          return _ember['default'].RSVP.resolve(new _ember['default'].RSVP.resolve([null, { options: { crs: crs32640 } }, [featureLayer1]]));
+        } else {
+          return _ember['default'].RSVP.resolve(new _ember['default'].RSVP.resolve([null, { options: { crs: crs32640 } }, [featureLayer2]]));
+        }
+      },
+      _getModelLeafletObject: function _getModelLeafletObject() {
+        return [null, { options: { crs: crs32640 } }];
+      }
+    });
+
+    var result = subject.differenceLayers('1', '2');
+
+    result.then(function (diff) {
+      assert.deepEqual(diff.diffFeatures[0].feature, {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: [[[8, 5], [6, 5], [6, 7], [3, 7], [3, 3], [8, 3], [8, 5]]]
+        }
+      });
+      assert.equal(diff.diffFeatures[0].area.toFixed(2), 16.00);
+      assert.ok(diff.layerA);
+      assert.ok(diff.layerB);
+      done();
+    });
+  });
+
+  (0, _qunit.test)('differenceLayers should return NOT difference', function (assert) {
+    assert.expect(1);
+    var done = assert.async(1);
+
+    var featureLayer1 = L.geoJSON(feature1).getLayers()[0];
+
+    var subject = mapApiMixinObject.create({
+      _getModelLayerFeature: function _getModelLayerFeature(layer) {
+        if (layer === '1') {
+          return _ember['default'].RSVP.resolve(new _ember['default'].RSVP.resolve([null, { options: { crs: crs32640 } }, [featureLayer1]]));
+        } else {
+          return _ember['default'].RSVP.resolve(new _ember['default'].RSVP.resolve([null, { options: { crs: crs32640 } }, [featureLayer1]]));
+        }
+      },
+      _getModelLeafletObject: function _getModelLeafletObject() {
+        return [null, { options: { crs: crs32640 } }];
+      }
+    });
+
+    var result = subject.differenceLayers('1', '2');
+
+    result.then(function (diff) {
+      assert.equal(diff, 'The difference is not found');
+      done();
+    });
+  });
+
+  (0, _qunit.test)('compareLayers should return array of objects', function (assert) {
+    assert.expect(22);
+    var done = assert.async(4);
+
+    var featureLayer1 = L.geoJSON(feature1).getLayers()[0];
+    featureLayer1.options.crs = crs32640;
+    featureLayer1.feature.properties.primarykey = '001';
+    var featureLayer3 = L.geoJSON(feature3).getLayers()[0];
+    featureLayer3.options.crs = crs32640;
+    featureLayer3.feature.properties.primarykey = '003';
+    var featureLayer4 = L.geoJSON(feature4).getLayers()[0];
+    featureLayer4.options.crs = crs32640;
+    featureLayer4.feature.properties.primarykey = '004';
+
+    var featureLayer2 = L.geoJSON(feature2).getLayers()[0];
+    featureLayer2.options.crs = crs32640;
+    featureLayer2.feature.properties.primarykey = '002';
+
+    var subject = mapApiMixinObject.create({
+      _getModelLayerFeature: function _getModelLayerFeature(layer) {
+        if (layer === '1') {
+          return _ember['default'].RSVP.resolve(new _ember['default'].RSVP.resolve([null, { options: { crs: crs32640 } }, [featureLayer1]]));
+        } else if ('2') {
+          return _ember['default'].RSVP.resolve(new _ember['default'].RSVP.resolve([null, { options: { crs: crs32640 } }, [featureLayer2, featureLayer3, featureLayer4]]));
+        }
+      },
+      _getModelLeafletObject: function _getModelLeafletObject() {
+        return [null, { options: { crs: crs32640 } }];
+      }
+    });
+
+    var resIntersects = subject.compareLayers('1', '2', 'intersects', false);
+    var resContains = subject.compareLayers('1', '2', 'contains', false);
+    var resNotIntersects = subject.compareLayers('1', '2', 'notIntersects', false);
+    var resNotDiff = subject.compareLayers('1', '1', 'intersects', false);
+
+    resIntersects.then(function (result) {
+      assert.equal(result.length, 2);
+      assert.deepEqual(result[0].objectDifference.feature, {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: [[[8, 5], [6, 5], [6, 7], [3, 7], [3, 3], [8, 3], [8, 5]]]
+        }
+      });
+      assert.equal(result[0].areaDifference.toFixed(2), 16.00);
+      assert.equal(result[0].id, '002');
+
+      assert.deepEqual(result[1].objectDifference.feature, {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: [[[4, 4], [4, 5], [5, 5], [5, 4], [4, 4]]]
+        }
+      });
+      assert.equal(result[1].areaDifference.toFixed(2), 1.00);
+      assert.equal(result[1].hasOwnProperty('id'), false);
+      done();
+    });
+
+    resContains.then(function (result) {
+      assert.equal(result.length, 2);
+      assert.deepEqual(result[0].objectDifference.feature, {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: [[[8, 5], [6, 5], [6, 7], [3, 7], [3, 3], [8, 3], [8, 5]]]
+        }
+      });
+      assert.equal(result[0].areaDifference.toFixed(2), 16.00);
+      assert.equal(result[0].hasOwnProperty('id'), false);
+
+      assert.deepEqual(result[1].objectDifference.feature, {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: [[[4, 4], [4, 5], [5, 5], [5, 4], [4, 4]]]
+        }
+      });
+      assert.equal(result[1].areaDifference.toFixed(2), 1.00);
+      assert.equal(result[1].id, '003');
+      done();
+    });
+
+    resNotIntersects.then(function (result) {
+      assert.equal(result.length, 2);
+      assert.deepEqual(result[0].objectDifference.feature, {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: [[[10, 9], [6, 9], [6, 7], [8, 7], [8, 5], [10, 5], [10, 9]]]
+        }
+      });
+      assert.equal(result[0].areaDifference.toFixed(2), 12.00);
+      assert.equal(result[0].id, '002');
+
+      assert.deepEqual(result[1].objectDifference.feature, {
+        type: 'Feature',
+        geometry: {
+          type: 'Polygon',
+          coordinates: [[[10, 3], [9, 3], [9, 2], [10, 2], [10, 3]]]
+        }
+      });
+      assert.equal(result[1].areaDifference.toFixed(2), 1.00);
+      assert.equal(result[1].id, '004');
+      done();
+    });
+
+    resNotDiff['catch'](function (result) {
+      assert.equal(result, 'The difference is not found');
+      done();
+    });
+  });
+});
+define('dummy/tests/unit/mixins/map-model-api-comparelayers-test.jscs-test', ['exports'], function (exports) {
+  'use strict';
+
+  module('JSCS - unit/mixins');
+  test('unit/mixins/map-model-api-comparelayers-test.js should pass jscs', function () {
+    ok(true, 'unit/mixins/map-model-api-comparelayers-test.js should pass jscs.');
+  });
+});
+define('dummy/tests/unit/mixins/map-model-api-comparelayers-test.jshint', ['exports'], function (exports) {
+  'use strict';
+
+  QUnit.module('JSHint - unit/mixins/map-model-api-comparelayers-test.js');
+  QUnit.test('should pass jshint', function (assert) {
+    assert.expect(1);
+    assert.ok(true, 'unit/mixins/map-model-api-comparelayers-test.js should pass jshint.');
   });
 });
 define('dummy/tests/unit/mixins/map-model-api-getmergegeometry-test-test', ['exports', 'ember', 'ember-flexberry-gis/mixins/flexberry-map-model-api', 'qunit'], function (exports, _ember, _emberFlexberryGisMixinsFlexberryMapModelApi, _qunit) {
