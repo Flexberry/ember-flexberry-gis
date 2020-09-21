662dcca0597a1a42e6e6349ce6e748d6855f730c
Flexberry-man <mail@flexberry.net>
2020-09-21 12:56:41 +0000

    Update gh-pages for develop-mssql branch

--- develop-mssql/assets/tests.js
+++ develop-mssql/assets/tests.js
@@ -5128,6 +5128,7 @@ define('dummy/tests/unit/mixins/flexberry-map-model-api-expansion-add-object-to-
 
   (0, _qunit.test)('test method addObjectToLayer with EPSG:4326', function (assert) {
     //Arrange
+    var done = assert.async(1);
     var ownerStub = _sinon['default'].stub(_ember['default'], 'getOwner');
     ownerStub.returns({
       knownForType: function knownForType() {
@@ -5149,20 +5150,25 @@ define('dummy/tests/unit/mixins/flexberry-map-model-api-expansion-add-object-to-
     var getMLObject = _sinon['default'].stub(subject, '_getModelLeafletObject', getModelLeafletObject);
 
     //Act
-    var result = subject.addObjectToLayer('1', geoJsonObject);
+    var promise = subject.addObjectToLayer('1', geoJsonObject);
 
-    //Assert
-    assert.equal(leafletObject.getLayers().length, 1);
-    assert.equal(result.layerId, '1');
-    assert.deepEqual(result._latlngs, [[L.latLng(0, 100), L.latLng(0, 101), L.latLng(1, 101), L.latLng(1, 100)]]);
-    assert.equal(getMLObject.callCount, 1, 'Check call count to method _getModelLeafletObject');
-    assert.equal(getMLObject.args[0][0], '1', 'Check call first arg to method _getModelLeafletObject');
-    ownerStub.restore();
-    getMLObject.restore();
+    assert.ok(promise instanceof _ember['default'].RSVP.Promise);
+
+    promise.then(function (result) {
+      assert.equal(leafletObject.getLayers().length, 0, 'Layers count');
+      assert.equal(result.layerId, '1', 'Layer id');
+      assert.deepEqual(result._latlngs, [[L.latLng(0, 100), L.latLng(0, 101), L.latLng(1, 101), L.latLng(1, 100)]]);
+      assert.equal(getMLObject.callCount, 1, 'Check call count to method _getModelLeafletObject');
+      assert.equal(getMLObject.args[0][0], '1', 'Check call first arg to method _getModelLeafletObject');
+      ownerStub.restore();
+      getMLObject.restore();
+      done();
+    });
   });
 
   (0, _qunit.test)('test method addObjectToLayer with EPSG:3395', function (assert) {
     //Arrange
+    var done = assert.async(1);
     var ownerStub = _sinon['default'].stub(_ember['default'], 'getOwner');
     ownerStub.returns({
       knownForType: function knownForType() {
@@ -5187,16 +5193,17 @@ define('dummy/tests/unit/mixins/flexberry-map-model-api-expansion-add-object-to-
     var getMLObject = _sinon['default'].stub(subject, '_getModelLeafletObject', getModelLeafletObject);
 
     //Act
-    var result = subject.addObjectToLayer('1', geoJsonObject, 'EPSG:3395');
-
-    //Assert
-    assert.equal(leafletObject.getLayers().length, 1);
-    assert.equal(result.layerId, '1');
-    assert.deepEqual(result._latlngs, [[L.latLng(0, 0.0008983152841195215), L.latLng(0, 0.0009072984369607167), L.latLng(0.00000904328947124462, 0.0009072984369607167), L.latLng(0.00000904328947124462, 0.0008983152841195215)]]);
-    assert.equal(getMLObject.callCount, 1, 'Check call count to method _getModelLeafletObject');
-    assert.equal(getMLObject.args[0][0], '1', 'Check call first arg to method _getModelLeafletObject');
-    ownerStub.restore();
-    getMLObject.restore();
+    subject.addObjectToLayer('1', geoJsonObject, 'EPSG:3395').then(function (result) {
+      //Assert
+      assert.equal(leafletObject.getLayers().length, 0);
+      assert.equal(result.layerId, '1');
+      assert.deepEqual(result._latlngs, [[L.latLng(0, 0.0008983152841195215), L.latLng(0, 0.0009072984369607167), L.latLng(0.00000904328947124462, 0.0009072984369607167), L.latLng(0.00000904328947124462, 0.0008983152841195215)]]);
+      assert.equal(getMLObject.callCount, 1, 'Check call count to method _getModelLeafletObject');
+      assert.equal(getMLObject.args[0][0], '1', 'Check call first arg to method _getModelLeafletObject');
+      ownerStub.restore();
+      getMLObject.restore();
+      done();
+    });
   });
 });
 define('dummy/tests/unit/mixins/flexberry-map-model-api-expansion-add-object-to-layer-test.jscs-test', ['exports'], function (exports) {
