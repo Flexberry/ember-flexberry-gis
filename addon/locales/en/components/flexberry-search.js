b63f3e1a32d3325f1bf27c6bc8978e3cce3a0353
SnezhanaAbramova <sabramova@skyori.ru>
2021-04-12 21:04:31 +0500

    rebrand search

--- addon/locales/en/components/flexberry-search.js
+++ addon/locales/en/components/flexberry-search.js
@@ -3,5 +3,15 @@ export default {
   'no-results': {
     'caption': 'No results',
     'description': 'Your search returned no results'
-  }
+  },
+  'search-properties': {
+    'caption': 'Attribute'
+  },
+  'search-layers': {
+    'caption': 'Layer'
+  },
+  'result-caption': 'Search results',
+  'error-message-caption': 'Search error',
+  'error-message-empty-selected-layer': 'Search layer isn\'t selected',
+  'error-message-empty-attr-layer': 'Search attribute isn\'t selected'
 };
