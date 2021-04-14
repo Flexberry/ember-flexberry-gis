d7d4c2586afb2690c5eb4cc7f1505652d3e754ea
DaryaNeko <natsume_maia@mail.ru>
2021-04-14 13:14:00 +0100

    jquery-ui

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ module.exports = {
     // Import extensions for jQuery 'resize' method ($('...').resize(...)).
     app.import('vendor/jquery/jquery.resize.extensions.js');
 
-    app.import(app.bowerDirectory + '/jquery-ui/jquery-ui.min.js');
-    app.import(app.bowerDirectory + '/jquery-ui/themes/base/resizable.css');
+    app.import('vendor/jquery/jquery-ui-draggable-resizable.min.css');
+    app.import('vendor/jquery/jquery-ui-draggable-resizable.min.js');
 
     // Import Leaflet library & it's resources.
     // Leaflet must be prepended (imported with prepend: true option),
