9e53651374a38dc5b84401205d86bb7e87c34238
iskakov <35532454+iskakov@users.noreply.github.com>
2021-06-08 15:01:10 +0500

    Now metadata layers visible on tree (#496)
    * Now metadata layers visible on tree
    
    * Добавил возможность добавления backgroundLayers
    
    * Поправил стили

--- addon/components/layers-dialogs/add.js
+++ addon/components/layers-dialogs/add.js
@@ -241,6 +241,8 @@ let FlexberryAddLayerDialogComponent = FlexberryEditLayerDialogComponent.extend(
               this.set('typeMessage', 'error');
             });
         }
+      } else {
+        this._super(...arguments);
       }
     },
 
