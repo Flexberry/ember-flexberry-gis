03a0851db120091ced037ccb35e0ce73b0d2204f
Alex Iskakov <ma3ca3@gmail.com>
2021-06-24 12:32:47 +0500

    Validate

--- tests/unit/mixins/favorites-features-test.js
+++ tests/unit/mixins/favorites-features-test.js
@@ -10,26 +10,22 @@ let editedLayerModel = Ember.A({ id: 11, crs: {} });
 
 let editedPolygon = L.polygon([[1, 2], [2, 3], [3, 4]]);
 editedPolygon.feature = editedPolygon.toGeoJSON();
-editedPolygon.toProjectedGeoJSON = () => { return editedPolygon.toGeoJSON(); };
 editedPolygon.feature.layerModel = editedLayerModel;
 editedPolygon.feature.properties.primarykey = 1;
 editedPolygon.feature.properties.prev = true;
 
 let firstFavoriteFeature = L.polygon([[10, 20], [20, 30], [30, 40]]);
-firstFavoriteFeature.toProjectedGeoJSON = () => { return firstFavoriteFeature.toGeoJSON(); };
 firstFavoriteFeature.feature = firstFavoriteFeature.toGeoJSON();
 firstFavoriteFeature.feature.layerModel = editedLayerModel;
 firstFavoriteFeature.feature.properties.primarykey = 2;
 
 let secondLayerModel = Ember.A({ id: 14, crs: {} });
 let secondFavoriteFeature = L.polygon([[11, 22], [22, 33], [33, 44]]);
-secondFavoriteFeature.toProjectedGeoJSON = () => { return secondFavoriteFeature.toGeoJSON(); };
 secondFavoriteFeature.feature = secondFavoriteFeature.toGeoJSON();
 secondFavoriteFeature.feature.layerModel = secondLayerModel;
 secondFavoriteFeature.feature.properties.primarykey = 3;
 
 let thirdFavoriteFeature = L.polygon([[2, 3], [3, 4], [5, 6]]);
-thirdFavoriteFeature.toProjectedGeoJSON = () => { return thirdFavoriteFeature.toGeoJSON(); };
 thirdFavoriteFeature.feature = thirdFavoriteFeature.toGeoJSON();
 thirdFavoriteFeature.feature.layerModel = secondLayerModel;
 thirdFavoriteFeature.feature.properties.primarykey = 3;
