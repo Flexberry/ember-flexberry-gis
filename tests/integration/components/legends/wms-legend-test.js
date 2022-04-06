e1d1a5c881470bd3dca24cd0038333d37d8e5494
DaryaNeko <natsume_maia@mail.ru>
2022-04-06 14:56:46 +0500

    test

--- tests/integration/components/legends/wms-legend-test.js
+++ tests/integration/components/legends/wms-legend-test.js
@@ -18,8 +18,8 @@ test('it renders', function(assert) {
   this.render(hbs`{{legends/wms-legend layer=testLayer}}`);
 
   let $images = this.$('img');
-  let expectedSrc = 'http://212.192.76.235:8080/geoserver/wms?service=WMS&request=GetLegendGraphic&version=1.1.0&format=image%2Fpng&layer=lesonas1969%3Agoszem&style=';
+  let src = 'http://212.192.76.235:8080/geoserver/wms?service=WMS&request=GetLegendGraphic&version=1.1.0&format=image%2Fpng&layer=lesonas1969%3Agoszem&style=';
 
   assert.equal($images.size(), 3, 'Images for all defined layers are rendered');
-  assert.equal($images.first().attr('src'), expectedSrc, 'Image\'s \'src\' attribute computed as expected');
+  assert.equal($images.first().attr('src'), src, 'Image\'s \'src\' attribute computed as expected');
 });
