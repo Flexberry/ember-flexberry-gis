95eca339fcff3aa286e2f94fc5afe7d69eb54acb
Alex Iskakov <ma3ca3@gmail.com>
2022-04-22 16:13:44 +0500

    fix serach panel

--- addon/components/flexberry-search-panel.js
+++ addon/components/flexberry-search-panel.js
@@ -37,10 +37,10 @@ export default Ember.Component.extend({
         `featuresPropertiesSettings.localizedProperties.${currentLocale}`) || {};
 
       let searchProperties = {};
-      Ember.keys(localizedProperties).forEach((prop) => {
-        if (searchFields.indexOf(prop) > -1) {
-          Ember.set(searchProperties, prop, localizedProperties[prop]);
-        }
+      searchFields.forEach((prop) => {
+        Ember.set(searchProperties, prop === 'length' ? prop + '1' : prop,
+          Ember.keys(localizedProperties).indexOf(prop) > -1 ?
+          localizedProperties[prop] : prop);
       });
 
       this.set('_localizedValue', null);
@@ -276,7 +276,7 @@ export default Ember.Component.extend({
       let searchProperties = this.get('_selectedLayerFeaturesLocalizedProperties');
       for (var property in searchProperties) {
         if (searchProperties[property] === selectedText) {
-          this.set('propertyName', property);
+          this.set('propertyName', property === 'length1' ? 'length' : property);
         }
       }
     },
