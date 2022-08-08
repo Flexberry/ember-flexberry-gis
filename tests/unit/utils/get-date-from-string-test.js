3d634391aa635102c78c985e8b4fe620374d241e
viatkinviatkin <svyatkin@skyori.ru>
2022-08-08 16:32:28 +0500

    fixed test

--- tests/unit/utils/get-date-from-string-test.js
+++ tests/unit/utils/get-date-from-string-test.js
@@ -17,15 +17,15 @@ test('getDateFormatFromString() test', function(assert) {
 test('createTimeInterval() test', function(assert) {
   let input = '12.01.2021 10:30:22';
   let timeInfo = getDateFormatFromString(input);
-  let date = moment(input, timeInfo.dateFormat + timeInfo.timeFormat);
+  let date = moment.utc(input, timeInfo.dateFormat + timeInfo.timeFormat);
   let [startInterval, endInterval] = createTimeInterval(date, timeInfo.dateFormat);
 
-  assert.equal('2021-01-12T05:30:22.000Z', startInterval);
-  assert.equal('2021-01-12T05:30:23.000Z', endInterval);
+  assert.equal('2021-01-12T10:30:22.000Z', startInterval);
+  assert.equal('2021-01-12T10:30:23.000Z', endInterval);
 
   let input2 = '2021-01-12T12:20:30.000Z';
   let timeInfo2 = getDateFormatFromString(input2);
-  let date2 = moment(input2, timeInfo2.dateFormat + timeInfo2.timeFormat);
+  let date2 = moment.utc(input2, timeInfo2.dateFormat + timeInfo2.timeFormat);
   let [startInterval2, endInterval2] = createTimeInterval(date2, timeInfo2.dateFormat);
   assert.equal('2021-01-12T12:20:30.000Z', startInterval2);
   assert.equal(undefined, endInterval2);
