4eee4d1501794eb326fafccd4f4b92435ffce8a3
viatkinviatkin <svyatkin@skyori.ru>
2022-08-09 15:34:57 +0500

    jscs fix

--- tests/unit/components/flexberry-layers-intersections-panel-test.js
+++ tests/unit/components/flexberry-layers-intersections-panel-test.js
@@ -95,7 +95,7 @@ test('test loadIntersectionLayers', function(assert) {
       ]
     }); // Tree of depth 2
 
-    let layerTree3 = Ember.Object.create({
+  let layerTree3 = Ember.Object.create({
       name: 'rootLayer',
       layers: [Ember.Object.create({
                 name:'layer1',
@@ -112,15 +112,15 @@ test('test loadIntersectionLayers', function(assert) {
               })
 
       ]
-    }); // Tree of depth 3
+  }); // Tree of depth 3
 
-    let testList1 = component.loadIntersectionLayers(emptyLayerTree.layers);
-    assert.equal(0, testList1.length, 'emptyList');
+  let testList1 = component.loadIntersectionLayers(emptyLayerTree.layers);
+  assert.equal(0, testList1.length, 'emptyList');
 
-    let testList2 = component.loadIntersectionLayers(layerTree1.layers);
-    assert.equal(2, testList2.length, 'layer tree depth 1 with 2 inner layers');
-    let testList3 = component.loadIntersectionLayers(layerTree2.layers);
-    assert.equal(3, testList3.length, 'layer tree depth 2 with 3 inner layers');
-    let testList4 = component.loadIntersectionLayers(layerTree3.layers);
-    assert.equal('layer1111', testList4[0].get('name'), 'layer tree depth 4 with 1 inner layer');
+  let testList2 = component.loadIntersectionLayers(layerTree1.layers);
+  assert.equal(2, testList2.length, 'layer tree depth 1 with 2 inner layers');
+  let testList3 = component.loadIntersectionLayers(layerTree2.layers);
+  assert.equal(3, testList3.length, 'layer tree depth 2 with 3 inner layers');
+  let testList4 = component.loadIntersectionLayers(layerTree3.layers);
+  assert.equal('layer1111', testList4[0].get('name'), 'layer tree depth 4 with 1 inner layer');
 });
