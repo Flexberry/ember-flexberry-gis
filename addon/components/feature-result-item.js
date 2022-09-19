df887875c78eb078a994c825fb48e7f0a5e2a3a6
viatkinviatkin <svyatkin@skyori.ru>
2022-09-19 18:27:55 +0500

    style correction

--- addon/components/feature-result-item.js
+++ addon/components/feature-result-item.js
@@ -177,6 +177,7 @@ export default Ember.Component.extend({
     if (this.feature.highlight) {
       this.feature.leafletLayer.setStyle({
         color: '#3388FF',
+        fillColor: 'salmon'
       });
       this.feature.leafletLayer.bringToFront();
     } else {
