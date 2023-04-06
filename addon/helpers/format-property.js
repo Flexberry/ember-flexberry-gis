df1937ba32448944b0a69a985fce44f00611eed4
DaryaNeko <natsume_maia@mail.ru>
2023-04-06 11:38:16 +0500

    Merge release210323 to develop (#708)
    * Ident fix (#702)
    
    * Ошибки идентификации
    
    * buffer, events
    
    * Поиск космоснимков
    
    ---------
    
    Co-authored-by: Lytkina Darya <DLytkina@skyori.ru>
    
    * 230311 fixed highlight fillColor (#699)
    
    * fixed highlight fillColor
    
    * "fill:true" is specified explicitly for feature-result-item
    
    ---------
    
    Co-authored-by: viatkinviatkin <svyatkin@skyori.ru>
    
    * fixed saving/editing a marker feature on the map (#700)
    
    Co-authored-by: viatkinviatkin <svyatkin@skyori.ru>
    
    * fixed fl-simpledatetime (#701)
    
    * fixed fl-simpledatetime
    
    * invalid date
    
    * test
    
    ---------
    
    Co-authored-by: viatkinviatkin <svyatkin@skyori.ru>
    Co-authored-by: Lytkina Darya <DLytkina@skyori.ru>
    
    * Fix edit layer component (#703)
    
    * fixed fl-simpledatetime
    
    * invalid date
    
    * test
    
    * Доработка результатов идентификации
    
    ---------
    
    Co-authored-by: viatkinviatkin <svyatkin@skyori.ru>
    Co-authored-by: Lytkina Darya <DLytkina@skyori.ru>
    
    * fix tests
    
    * Fix small help popup styles (#704)
    
    * refactor (#705)
    
    Co-authored-by: Lytkina Darya <DLytkina@skyori.ru>
    
    * tests
    
    * tests
    
    * fixed point geometry update for _updateFeatureResultItem (#706)
    
    Co-authored-by: viatkinviatkin <svyatkin@skyori.ru>
    
    * Ошибки (#707)
    
    * Ошибки
    
    * tests
    
    ---------
    
    Co-authored-by: Lytkina Darya <DLytkina@skyori.ru>
    
    * Fix styles (#709)
    
    * Fix styles (#710)
    
    * fixes (#712)
    
    Co-authored-by: Lytkina Darya <DLytkina@skyori.ru>
    
    * tests
    
    ---------
    
    Co-authored-by: Lytkina Darya <DLytkina@skyori.ru>
    Co-authored-by: viatkinviatkin <49162494+viatkinviatkin@users.noreply.github.com>
    Co-authored-by: viatkinviatkin <svyatkin@skyori.ru>
    Co-authored-by: pepelyaeva <kpepelyaeva@neoplatform.ru>

---
+++ addon/helpers/format-property.js
@@ -0,0 +1,51 @@
+import Ember from 'ember';
+import moment from 'moment';
+
+export default Ember.Helper.extend({
+  dateFormat: 'DD.MM.YYYY',
+
+  compute(args) {
+    let value = args[0];
+
+    if (Ember.isNone(value)) {
+      return value;
+    }
+
+    let type = args[1];
+    let settings = args[2] || {};
+
+    switch (type) {
+      case 'date':
+        let dateFormat = settings.dateFormat || this.get('dateFormat');
+        let dateTimeFormat = settings.dateTimeFormat;
+
+        if (!Ember.isEmpty(dateFormat) || !Ember.isEmpty(dateTimeFormat)) {
+          let dateValue = moment(value);
+
+          if (dateValue.isValid()) {
+            if (!Ember.isEmpty(dateTimeFormat)) {
+              return (dateValue.format('HH:mm:ss') === '00:00:00') ? dateValue.format(dateFormat) : dateValue.format(dateTimeFormat);
+            } else {
+              return dateValue.format(dateFormat);
+            }
+          }
+        }
+
+        break;
+      case 'boolean':
+        let i18n = this.get('i18n');
+        let yes = i18n.t('components.layer-result-list.boolean.yes');
+        let no = i18n.t('components.layer-result-list.boolean.no');
+
+        return (typeof (value) === 'boolean') ? (value) ? yes : no : (value === 'true') ? yes : no;
+      case 'number':
+        return value.toFixed(2).replace(/./g, function (c, i, a) {
+          return i && c !== '.' && ((a.length - i) % 3 === 0) ? ' ' + c : c;
+        });
+      default:
+        return value;
+    }
+
+    return value;
+  }
+});
