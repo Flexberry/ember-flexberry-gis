4b4e4596f6b53a0f5b975f95de09cb5736daafed
Vasilii Ryndin <vryndin@pminst.ru>
2025-05-21 10:50:11 +0300

    Add successModal

---
+++ addon/services/modal-message.js
@@ -0,0 +1,60 @@
+import Ember from "ember";
+
+export default Ember.Service.extend({
+  show: false,
+  messageCaption: "",
+  messageText: "",
+  applicationController: null,
+
+  init() {
+    this._super(...arguments);
+    const container = Ember.getOwner(this).lookup("controller:application");
+    this.set("applicationController", container);
+  },
+
+  showModal(options) {
+    const controller = this.get("applicationController");
+
+    const onClose = function () {
+      controller.set("isModalVisible", false);
+      if (options.onClose) {
+        options.onClose();
+      }
+    };
+
+    controller.setProperties({
+      isModalVisible: true,
+      modalTitle: options.title,
+      modalText: options.text,
+      modalDuration: options.duration || 5000,
+      modalOnClose: onClose,
+      modalExtraClass: options.extraClass || "",
+    });
+  },
+
+  showErrorModal(options) {
+    const controller = this.get("applicationController");
+
+    controller.setProperties({
+      isErrorModalVisible: true,
+      errorModalTitle: options.title,
+      errorModalText: options.text,
+    });
+  },
+
+  setModalMessage(caption, text) {
+    this.setProperties({
+      show: true,
+      messageCaption: caption,
+      messageText: text,
+    });
+  },
+
+  clearModalMessage() {
+    this.setProperties({
+      show: false,
+      messageCaption: "",
+      messageText: "",
+    });
+  },
+});
