a8ad01a0a467ed24907b7a69584d3dd5846cb768
viatkinviatkin <svyatkin@skyori.ru>
2025-11-19 00:07:46 +0500

    Реализован layer-export компонент

--- addon/components/layer-export.js
+++ addon/components/layer-export.js
@@ -1,5 +1,15 @@
 import Ember from 'ember';
 import layout from '../templates/components/layer-export';
+import { downloadBlob } from '../utils/download-file';
+
+const ExportStatus = {
+  PENDING: 0,
+  RUNNING: 1,
+  COMPLETED: 2,
+  FAILED: 3,
+  CANCELLED: 4,
+  TIMEDOUT: 5,
+};
 
 export default Ember.Component.extend({
   layout,
@@ -29,12 +39,25 @@ export default Ember.Component.extend({
     GML2: 'GML2',
     GML3: 'GML3',
     KML: 'KML',
-    'Shape Zip': 'Shape Zip',
-    MIF: 'MIF',
+    'ESRI Shapefile': 'Shape Zip',
+    'MapInfo File': 'MIF',
+    GPX: 'GPX',
   },
 
   noGeometryFormats: {
-    Excel: 'Excel (без геометрии)',
+    XLSX: 'Excel (без геометрии)',
+  },
+
+  fileExtensions: {
+    GeoJSON: 'geojson',
+    CSV: 'csv',
+    GML2: 'gml',
+    GML3: 'gml',
+    KML: 'kml',
+    'ESRI Shapefile': 'shp',
+    'MapInfo File': 'mif',
+    GPX: 'gpx',
+    XLSX: 'xlsx',
   },
 
   format: 'GeoJSON',
@@ -57,59 +80,132 @@ export default Ember.Component.extend({
 
   selectedCRS: 'EPSG:4326',
 
+  intervalID: null,
+  intervalTime: 1000,
+
   init() {
     this._super(...arguments);
 
     const layer = this.get('layer');
+    const crs = layer.get('crs.code');
 
-    if (layer) {
-      this.set('selectedCRS', layer.get('crs.code'));
-    }
+    this.set('selectedCRS', crs);
+  },
+
+  start(param) {
+    Ember.$.ajax({
+      url: `${config.APP.backendUrls.exportApi}`,
+      type: 'POST',
+      data: JSON.stringify(param),
+      dataType: 'json',
+      contentType: 'application/json; charset=utf-8',
+    })
+      .done((response) => {
+        const exportID = response;
+        const intervalID = setInterval(() => this.poll(exportID), this.get('intervalTime'));
+        this.set('intervalID', intervalID);
+        this.send('onExecute');
+      })
+      .fail((error) => {
+        console.error({
+          url: config.APP.backendUrls.exportApi,
+          message: error.message,
+          error,
+        });
+
+        this.send('onError');
+      });
+  },
+
+  poll(exportID) {
+    const intervalID = this.get('intervalID');
+
+    Ember.$.ajax({
+      url: `${config.APP.backendUrls.exportApi}/${exportID}/status`,
+      type: 'GET',
+      cache: false,
+      dataType: 'json',
+    })
+      .done((response) => {
+        if (!response) {
+          console.error({ message: 'No response' });
+          clearInterval(intervalID);
+          this.send('onError');
+          return;
+        }
+
+        // TODO: errorMessage может появиться для статуса ExportStatus.running - исправить
+        if (response.status === ExportStatus.FAILED || response.status === ExportStatus.CANCELLED || response.status === ExportStatus.TIMEDOUT) {
+          console.error({ message: response.errorMessage || 'Unexpected export polling error', response: response });
+          clearInterval(intervalID);
+          this.send('onError');
+          return;
+        }
+
+        // Успешное завершение
+        if (response.status === ExportStatus.COMPLETED) {
+          this.download(exportID);
+          clearInterval(intervalID);
+          return;
+        }
+
+        // Продолжаем опрашивать статус
+      })
+      .fail((e) => {
+        console.error(e);
+        clearInterval(intervalID);
+        this.send('onError');
+      });
+  },
+
+  download(exportID) {
+    Ember.$.ajax({
+      url: `${config.APP.backendUrls.exportApi}/${exportID}/file`,
+      method: 'GET',
+      cache: false,
+      xhrFields: {
+        responseType: 'blob',
+      },
+
+      success: (blob) => {
+        const layerName = this.get('layer.name');
+        const fileExtension = Ember.get(this.get('fileExtensions'), this.get('format'));
+        const fileName = layerName + '.' + fileExtension;
+
+        downloadBlob(fileName, blob);
+        this.send('onSuccess');
+      },
+      error: (error) => {
+        console.error({ message: error });
+        this.send('onError');
+      },
+    });
   },
 
   actions: {
     onApprove() {
-      const type = this.get('layer.type');
       let settings = this.get('layer.settingsAsObject');
 
-      if (type === 'wms-wfs') {
+      if (this.get('layer.type') === 'wms-wfs') {
         settings = settings.wfs;
       }
 
       const data = {
         OutputFormat: this.get('format'),
         LayerName: `${settings.typeNS}:${settings.typeName}`,
+        SourceSrs: this.get('selectedCRS') ? this.get('layer.crs.code') : null,
         TargetSrs: this.get('selectedCRS'),
-        //SourceConnectionString: this.layer.get('settingsAsObject.url'),
-        AdditionalArguments: '-limit 5 --config GDAL_HTTP_UNSAFESSL YES --config GDAL_HTTP_VERIFYSSL NO',
+        LayerType: this.get('layer.type'),
+        AdditionalArguments: ` -unsetFid -noNativeData -nomd --config GDAL_HTTP_UNSAFESSL YES --config GDAL_HTTP_VERIFYSSL NO -dsco FORMAT=GML3 -oo EXPOSE_GML_ID=NO`,
         //geometryField: this.layer.get('settingsAsObject.geometryField'),
       };
 
-      Ember.$.ajax({
-        url: `${config.APP.backendUrls.layerExportApi}`,
-        type: 'POST',
-        data: JSON.stringify(data),
-        dataType: 'json',
-        contentType: 'application/json',
-      })
-        .done((response) => {
-          if (response) {
-            console.log(response);
-          }
-        })
-        .fail(() => {
-          this.get('modalMessage').showModal({
-            title: 'Внимание',
-            text: `Слой не удалось выгрузить по техническим причинам. Попробуйте еще раз позднее. Если проблема повторится, обратитесь в техподдержку`,
-            autoClose: false,
-            modalIconClass: 'icon-guideline-circle-info red',
-          });
-        });
+      this.start(data);
     },
 
     onTypeChange(newValue) {
       if (newValue === 'withoutGeometry') {
-        this.set('format', 'Excel');
+        this.set('format', 'XLSX');
         this.set('selectedCRS', null);
       }
 
@@ -119,25 +215,31 @@ export default Ember.Component.extend({
       }
     },
 
-    // this.get('modalMessage').showModal({
-    //   title: 'Внимание',
-    //   text: `Слой не удалось выгрузить по техническим причинам. Попробуйте еще раз позднее. Если проблема повторится, обратитесь в техподдержку`,
-    //   autoClose: false,
-    //   modalIconClass: 'icon-guideline-circle-info red',
-    // });
+    onError() {
+      this.get('modalMessage').showModal({
+        title: 'Внимание',
+        text: `Слой не удалось выгрузить по техническим причинам. Попробуйте еще раз позднее. Если проблема повторится, обратитесь в техподдержку`,
+        autoClose: false,
+        modalIconClass: 'icon-guideline-circle-info red',
+      });
+    },
 
-    // this.get('modalMessage').showModal({
-    //   title: '',
-    //   text: `Экспорт данных запущен`,
-    //   modalIconClass: 'icon-guideline-circle-more orange',
-    //   autoClose: false,
-    // });
+    onSuccess() {
+      this.get('modalMessage').showModal({
+        title: '',
+        modalIconClass: 'icon-guideline-circle-check green',
+        text: `Экспорт данных успешно завершён`,
+        autoClose: false,
+      });
+    },
 
-    // this.get('modalMessage').showModal({
-    //   title: '',
-    //   modalIconClass: 'icon-guideline-circle-check green',
-    //   text: `Экспорт данных успешно завершён`,
-    //   autoClose: false,
-    // });
+    onExecute() {
+      this.get('modalMessage').showModal({
+        title: '',
+        text: `Экспорт данных запущен`,
+        modalIconClass: 'icon-guideline-circle-more orange',
+        autoClose: false,
+      });
+    },
   },
 });
